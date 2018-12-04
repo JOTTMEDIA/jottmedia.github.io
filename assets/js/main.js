8744d90de44d654d33741d840b9f7734b0c11370
Jonathan Schneider <jonathan@jott.media>
2018-12-04 12:41:53 +0100

    add css und js base

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,42 @@
-!(function (window, $) {
+var loadScene;
+(function (window, document, $) {
 
-})(window, jQuery)
+  $(function () {
+
+    // init controller
+    var controller = new ScrollMagic.Controller({ refreshInterval: 0 });
+    loadScene = {
+      main: {
+        reference: [],
+        scene1: function () {
+          // var tween = new TimelineMax()
+          //   .to("div#about section#one div.fade div:nth-child(1)", 1, { opacity: 1 }).to("div#about section#one div.fade div:nth-child(1)", 1, { opacity: 0 })
+          //   .to("div#about section#one div.fade div:nth-child(2)", 1, { opacity: 1 }).to("div#about section#one div.fade div:nth-child(2)", 1, { opacity: 0 })
+          //   .to("div#about section#one div.fade div:nth-child(3)", 1, { opacity: 1 });
+          // var scene = new ScrollMagic.Scene({ triggerElement: "div#about section#one", triggerHook: 0, duration: "750px" })
+          //   .setTween(tween)
+          //   .setPin("div#about section#one")
+          //   // .addIndicators({name: "Section ABOUT 1"})
+          //   .addTo(controller);
+
+          loadScene.about.reference.push(scene);
+        },
+        init: function () {
+          // console.log("INIT ABOUT");
+          loadScene.about.scene1();
+        },
+        destroy: function () {
+          // console.log(loadScene.about.reference);
+          loadScene.about.reference.forEach(function (scene) {
+            scene.destroy(true);
+          });
+          loadScene.about.reference = [];
+        }
+      }
+    };
+
+    loadScene.main.init();
+
+  });
+
+})(window, document, jQuery);
