c046a01f2bd7ead64bde1dacac08e1962d6beadf
Jonathan Schneider <jonathan@jott.media>
2018-12-04 14:34:10 +0100

    section 1 writing animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,32 +10,69 @@ var loadScene;
         reference: [],
         scene1: function () {
           // var tween = new TimelineMax()
-          //   .to("div#about section#one div.fade div:nth-child(1)", 1, { opacity: 1 }).to("div#about section#one div.fade div:nth-child(1)", 1, { opacity: 0 })
-          //   .to("div#about section#one div.fade div:nth-child(2)", 1, { opacity: 1 }).to("div#about section#one div.fade div:nth-child(2)", 1, { opacity: 0 })
-          //   .to("div#about section#one div.fade div:nth-child(3)", 1, { opacity: 1 });
-          // var scene = new ScrollMagic.Scene({ triggerElement: "div#about section#one", triggerHook: 0, duration: "750px" })
+          //   .to("div#main section#one div.fade div:nth-child(1)", 1, { opacity: 1 }).to("div#main section#one div.fade div:nth-child(1)", 1, { opacity: 0 })
+          //   .to("div#main section#one div.fade div:nth-child(2)", 1, { opacity: 1 }).to("div#main section#one div.fade div:nth-child(2)", 1, { opacity: 0 })
+          //   .to("div#main section#one div.fade div:nth-child(3)", 1, { opacity: 1 });
+
+          // var scene = new ScrollMagic.Scene({ triggerElement: "div#main section#one", triggerHook: 0, duration: "750px" })
           //   .setTween(tween)
-          //   .setPin("div#about section#one")
-          //   // .addIndicators({name: "Section ABOUT 1"})
+          //   .setPin("div#main section#one")
+          //   // .addIndicators({name: "Section main 1"})
           //   .addTo(controller);
 
-          loadScene.about.reference.push(scene);
+          var text = [
+            "Wir gestalten, konzipieren, programmieren und visualisieren Medien.",
+            "Wir sind JOTT.MEDIA – Eine Digital- & Print-Agentur aus Schaumburg.",
+            "Was? Noch am lesen? Bitte scrollen."
+          ];
+          startTextAnimation(0, text, "section:nth-child(1) div.text");
+
+          // loadScene.main.reference.push(scene1);
         },
         init: function () {
-          // console.log("INIT ABOUT");
-          loadScene.about.scene1();
+          loadScene.main.scene1();
         },
         destroy: function () {
-          // console.log(loadScene.about.reference);
-          loadScene.about.reference.forEach(function (scene) {
-            scene.destroy(true);
-          });
-          loadScene.about.reference = [];
+          // loadScene.main.reference.forEach(function (scene) {
+          //   scene.destroy(true);
+          // });
+          // loadScene.main.reference = [];
         }
       }
     };
 
     loadScene.main.init();
+
+
+    /**
+     * Helper
+     */
+
+    function typeWriter(text, i, selector, fnCallback) {
+      if (i < (text.length)) {
+        $(selector).html(text.substring(0, i + 1) + '<span class="cursor"></span>');
+
+        setTimeout(function () {
+          typeWriter(text, i + 1, selector, fnCallback)
+        }, 100);
+      }
+      else if (typeof fnCallback == "function") {
+        setTimeout(fnCallback, 700);
+      }
+    }
+
+    function startTextAnimation(i, dataText, selector) {
+      if (typeof dataText[i] == "undefined") {
+        setTimeout(function () {
+          startTextAnimation(0, dataText, selector);
+        }, 20000);
+      } else {
+        typeWriter(dataText[i], 0, selector, function () {
+          startTextAnimation(i + 1, dataText, selector);
+        });
+      }
+    }
+
 
   });
 
