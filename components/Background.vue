9c5e39045c2e4b05e98e80247adf408af99181a1
Jonathan Schneider <jonathan@jott.media>
2023-12-14 18:43:17 +0100

    feat(Footer.vue, Background.vue): add new components and assets (relaunch-2024)
    This commit includes changes to `Footer.vue` and a new component `Background.vue`.
    
    In `Footer.vue` the structure was updated and now includes the `Background.vue` component for enhanced styling.
    
    The new `Background.vue` component receives a number of props to handle the positioning, height, width, and source of a background image.
    
    Additionally, two images `footer-top.svg` and `box.png` were added to the assets.
    
    These changes contribute to the application's relaunch for 2024.

---
+++ components/Background.vue
@@ -0,0 +1,57 @@
+<template>
+  <div :style="{backgroundImage: `url('${getImageAbsolutePath(src)}')`, height, width}" :class="[out ? positionToClassOut[position] : positionToClassIn[position]]" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  height: {
+    type: String,
+    required: true,
+  },
+  width: {
+    type: String,
+    default: null,
+  },
+  src: {
+    src: String,
+    required: true,
+  },
+  position: {
+    type: String,
+    default: 'top',
+  },
+  out: {
+    type: Boolean,
+    default: true,
+  },
+})
+
+const positionToClassOut = {
+  top: 'absolute top-0 -translate-y-full repeat-x bg-center w-full',
+  topLeft: 'absolute top-0 left-0 -translate-y-full bg-no-repeat bg-contain bg-left-top',
+  topRight: 'absolute top-0 right-0 -translate-y-full bg-no-repeat bg-contain bg-right-top',
+  center: 'absolute top-50 -translate-y-1/2 repeat-x bg-center w-full',
+  centerLeft: 'absolute top-50 left-0 -translate-y-1/2 bg-no-repeat bg-contain bg-left-center',
+  centerRight: 'absolute top-50 right-0 -translate-y-1/2 bg-no-repeat bg-contain bg-right-center',
+  bottom: 'absolute bottom-0 translate-y-full repeat-x bg-center w-full',
+  bottomLeft: 'absolute bottom-0 left-0 translate-y-full bg-no-repeat bg-contain bg-left-bottom',
+  bottomRight: 'absolute bottom-0 right-0 translate-y-full bg-no-repeat bg-contain bg-right-bottom',
+}
+
+const positionToClassIn = {
+  top: 'absolute top-0 repeat-x bg-center w-full',
+  topLeft: 'absolute top-0 left-0 bg-no-repeat bg-contain bg-left-top',
+  topRight: 'absolute top-0 right-0 bg-no-repeat bg-contain bg-right-top',
+  center: 'absolute top-50 -translate-y-1/2 repeat-x bg-center w-full',
+  centerLeft: 'absolute top-50 left-0 -translate-y-1/2 bg-no-repeat bg-contain bg-left-center',
+  centerRight: 'absolute top-50 right-0 -translate-y-1/2 bg-no-repeat bg-contain bg-right-center',
+  bottom: 'absolute bottom-0 repeat-x bg-center w-full',
+  bottomLeft: 'absolute bottom-0 left-0 bg-no-repeat bg-contain bg-left-bottom',
+  bottomRight: 'absolute bottom-0 right-0 bg-no-repeat bg-contain bg-right-bottom'
+}
+
+const glob = import.meta.glob<Record<string, string>>('@/assets/images/*', { eager: true })
+const getImageAbsolutePath = (imageName: string): string | undefined => {
+  return glob[`/assets/images/${imageName}`]['default'];
+};
+</script>
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,27 +1,33 @@
 <template>
-  <div class="bg-jm-secondary-grey-lighter">
-    <UContainer class="py-10" :ui="{'constrained': 'max-w-4xl'}">
-      <Instagram />
-    </UContainer>
+  <div class="relative bg-jm-secondary-grey-lighter">
+    <Background height="178px" src="footer-top.svg" position="top" :out="false" />
 
-    <UContainer class="py-10" :ui="{'constrained': 'max-w-2xl'}">
-      <div class="flex justify-between items-end">
-        <div>
-          <address class="not-italic">
-            JOTT.MEDIA GmbH<br>
-            Bahnhofstraße 33<br>
-            31675 Bückeburg<br>
-            <br>
-            <nuxt-link href="tel:+4957229979070">+49 5722 99790 70</nuxt-link><br>
-            <nuxt-link href="mailto:hallo@jott.media">hallo@jott.media</nuxt-link>
-          </address>
+    <div class="relative z-10">
+      <UContainer class="relative py-10 z-10" :ui="{'constrained': 'max-w-4xl'}">
+        <Instagram />
+      </UContainer>
+
+      <UContainer class="relative py-10 z-10" :ui="{'constrained': 'max-w-2xl'}">
+        <div class="flex justify-between items-end">
+          <div>
+            <address class="not-italic">
+              JOTT.MEDIA GmbH<br>
+              Bahnhofstraße 33<br>
+              31675 Bückeburg<br>
+              <br>
+              <nuxt-link href="tel:+4957229979070">+49 5722 99790 70</nuxt-link><br>
+              <nuxt-link href="mailto:hallo@jott.media">hallo@jott.media</nuxt-link>
+            </address>
+          </div>
+          <div class="text-right">
+            <nuxt-link to="/imprint" class="block font-[800] text-jm-primary-green uppercase">.Impressum</nuxt-link>
+            <nuxt-link to="/privacy" class="block font-[800] text-jm-primary-green uppercase">.Datenschutz</nuxt-link>
+          </div>
         </div>
-        <div class="text-right">
-          <nuxt-link to="/imprint" class="block font-[800] text-jm-primary-green uppercase">.Impressum</nuxt-link>
-          <nuxt-link to="/privacy" class="block font-[800] text-jm-primary-green uppercase">.Datenschutz</nuxt-link>
-        </div>
-      </div>
-    </UContainer>
+      </UContainer>
+
+      <Background width="800px" height="500px" src="box.png" position="bottomRight" :out="false" />
+    </div>
 
     <div class="bg-jm-primary-green text-sm py-5">
       <UContainer :ui="{'constrained': 'max-w-2xl'}">
@@ -30,3 +36,5 @@
     </div>
   </div>
 </template>
+<script setup lang="ts">
+</script>
