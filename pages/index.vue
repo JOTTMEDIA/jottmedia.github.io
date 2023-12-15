1b0924e6408e62bd780be2d69878236e548a5ed3
Jonathan Schneider <jonathan@jott.media>
2023-12-15 17:40:01 +0100

    feat(text): update

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
           </Center>
 
           <Center>
-            <Headline type="h1" class="pb-5">„Machen wir es<br><span class="text-jm-primary-green uppercase">einfach:</span> <b class="uppercase">digital</b>.“</Headline>
+            <Headline type="h1" class="pb-5">Machen wir es<br><span class="text-jm-primary-green uppercase">einfach:</span> <b class="uppercase">digital</b>.</Headline>
             <NuxtLink :to="{ path: '/', hash: '#mission' }">
               <Button>Einfach machen</Button>
             </NuxtLink>
@@ -33,21 +33,22 @@
           <Headline type="h6">Das bekommst du <b class="uppercase">von uns</b></Headline>
           <Headline type="h4"><b>Mehr Zeit</b>, sodass du deiner Energie auf den <b>Erfolg</b> und das Wachstum deines Unternehmens fokussieren kannst. Dazu vereinfachen und digitalisieren wir deine Prozesse – und sorgen so für <b>mehr Effizienz, Zufriedenheit und Qualität</b> für dein Business.</Headline>
         </UContainer>
-        <Background height="876px" src="header-green-bottom.svg" position="bottom" />
+        <Background height="875px" src="header-green-bottom.svg" position="bottom" />
       </div>
 
       <UContainer class="relative py-10" :ui="{'constrained': 'max-w-4xl'}" style="perspective: 3000px">
         <NuxtImg ref="image" :style="imageStyle" src="https://picsum.photos/1024/768" alt="" class="w-full block" />
       </UContainer>
 
-      <UContainer class="relative py-10" :ui="{'constrained': 'max-w-2xl'}">
-        <Headline type="h3" class="font-[400]">Denn wir entwickeln und designen <b class="text-jm-primary-brown">digitale Premiumlösungen, die</b> <b>nur für dein Unternehmen</b> <b class="text-jm-primary-brown">passen.</b></Headline>
+      <UContainer id="question" class="relative py-10" :ui="{'constrained': 'max-w-2xl'}">
+        <Headline type="h6">Die wichtigste <b class="uppercase">Frage</b></Headline>
+        <Headline type="h2" class="font-[400]">„<b class="uppercase">Was</b> genau <b class="uppercase">braucht dein Unternehmen wirklich?</b>“</Headline>
       </UContainer>
 
       <UContainer id="leistungen" class="relative py-10" :ui="{'constrained': 'max-w-2xl'}">
-        <Headline type="h6">Unsere <b class="uppercase">Leistungen</b></Headline>
-        <Headline type="h5" class="text-jm-primary-brown"><b>Konzeption, Prototyping, Programmierung & Design</b></Headline>
-        <Paragraph>Von komplexen, automatisierten und <b>individuell</b> zugeschnittenen <b>Websites & Apps</b> mit einem hohen Anspruch an <b>Funktionalität, Userfreundlichkeit</b> und <b>Ästhetik.</b> Weiterer Text mit dem Aufgreifen von z. B. UI und UX Design und genaureres zu Prototyping von Apps und Web-Applikationen. Mehr Details zu euren Leistungen und Arbeitsvorgängen.</Paragraph>
+        <Headline type="h6">So arbeiten wir <b class="uppercase">für Dich</b></Headline>
+        <Headline type="h5" class="text-jm-primary-brown"><b>Ehrlich. zuverlässig. partnerschaftlich.</b></Headline>
+        <Paragraph>Die Lösung muss immer zum Problem passen. Darum steht bei uns <b>an erster Stelle</b> die Analyse deiner Herausforderung. Erst, wenn diese glasklar ist, widmen wir uns der <b>Konzeption und Umsetzung</b> des tatsächlichen Produkts, wie auch immer das in deinem konkreten Fall aussehen mag. Damit das funktioniert, ist eine ehrliche, zuverlässige und partnerschaftliche Zusammenarbeit – <b>auf Augenhöhe</b> und auf beiden Seiten – unabdingbar.</Paragraph>
         <Center>
           <NuxtLink href="https://calendar.app.google/rBDjAnPNYEQpfMvJ9" target="_blank">
             <Button>Lass uns gemeinsam loslegen</Button>
@@ -59,11 +60,11 @@
         <Background height="815px" src="grey.svg" position="top" :out="false" />
         <Background height="1397px" src="sand-grey.svg" position="top" :out="false" />
         <UContainer class="relative py-10 md:mb-40 z-10" :ui="{'constrained': 'max-w-4xl'}">
-          <ImageFigure media-url="https://picsum.photos/1800/1800" hint="Jonathan, CTO" align="left">„<b class="text-jm-primary-brown">Mich begeistert</b> die unendliche Kreativität, mit meinen Händen und meinem MacBook <b>die Welt zu verändern.</b>“</ImageFigure>
-          <ImageFigure media-url="https://picsum.photos/1800/1800" hint="Jan, CCO" align="right">„<b class="text-jm-primary-brown">Mein Mut zur Veränderung</b> und somit unsere Kompetenzen ergänzend nutzen zu können, <b>war die beste Entscheidung</b> meines Lebens.“</ImageFigure>
-          <ImageFigure media-url="https://picsum.photos/1800/1800" hint="Arian, Developer" align="left">„<b class="text-jm-primary-brown">Mich begeistert</b> die unendliche Kreativität, mit meinen Händen und meinem MacBook <b>die Welt zu verändern.</b>“</ImageFigure>
-          <ImageFigure media-url="https://picsum.photos/1800/1800" hint="Annika, Graphic Designer" align="right">„<b class="text-jm-primary-brown">Mich begeistert</b> die unendliche Kreativität, mit meinen Händen und meinem MacBook <b>die Welt zu verändern.</b>“</ImageFigure>
-          <ImageFigure media-url="https://picsum.photos/1800/1800" hint="Inke, CFO" align="left">„<b class="text-jm-primary-brown">Mich begeistert</b> die unendliche Kreativität, mit meinen Händen und meinem MacBook <b>die Welt zu verändern.</b>“</ImageFigure>
+          <ImageFigure media-url="https://picsum.photos/1800/1800" hint="Jonathan // Entwicklung" align="left">„<b class="text-jm-primary-brown">Mich begeistert</b> die unendliche Kreativität, mit meinen Händen und meinem MacBook <b>die Welt zu bereichern.</b>“</ImageFigure>
+          <ImageFigure media-url="https://picsum.photos/1800/1800" hint="Jan // Gestaltung" align="right">„<b class="text-jm-primary-brown">Mein Mut zur Veränderung</b> und somit unsere Kompetenzen ergänzend nutzen zu können, <b>war die beste Entscheidung</b> meines Lebens.“</ImageFigure>
+          <ImageFigure media-url="https://picsum.photos/1800/1800" hint="Arian // Entwicklung" align="left">„<b class="text-jm-primary-brown">Es gibt</b> keine bessere Schönheit als <b>den Intellekt.</b>“</ImageFigure>
+          <ImageFigure media-url="https://picsum.photos/1800/1800" hint="Annika // Gestaltung" align="right">„<b class="text-jm-primary-brown">Schon als Kind</b> war ich immer <b>fasziniert</b> von farben, formen und mustern und habe es geliebt, <b>neue Dinge zu erschaffen.</b>“</ImageFigure>
+          <ImageFigure media-url="https://picsum.photos/1800/1800" hint="Inke // Buchhaltung" align="left">„<b class="text-jm-primary-brown">Obwohl</b> in meiner Schulzeit zuerst die <b>Mengenlehre</b> unterrichtet wurde, <b>sind</b> die <b>Zahlen mein</b> ständiger <b>Begleiter</b> geworden.“</ImageFigure>
         </UContainer>
         <Background height="854px" src="sand.svg" position="bottom" :out="false" />
       </div>
@@ -76,7 +77,7 @@
         </Center>
       </UContainer>
 
-      <div class="relative bg-jm-primary-green">
+      <div class="relative bg-jm-primary-green mt-64">
         <Background height="703px" src="green-top.svg" position="top" />
         <UContainer id="argument" class="relative py-10" :ui="{'constrained': 'max-w-2xl'}">
           <Headline type="h6">Unser <b class="uppercase">Argument</b></Headline>
