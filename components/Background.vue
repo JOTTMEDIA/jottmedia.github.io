714c93fbaaa6a947743b76b1efaf10e1f553f51e
Jonathan Schneider <jonathan@jott.media>
2023-12-18 16:27:21 +0100

    feat(images): update

--- components/Background.vue
+++ components/Background.vue
@@ -45,27 +45,27 @@ const targetStyle = computed(() => {
 })
 
 const positionToClassOut = {
-  top: 'absolute top-0 -translate-y-full repeat-x bg-center w-screen',
-  topLeft: 'absolute top-0 left-0 -translate-y-full bg-no-repeat bg-contain bg-left-top',
-  topRight: 'absolute top-0 right-0 -translate-y-full bg-no-repeat bg-contain bg-right-top',
+  top: 'absolute top-[1px] -translate-y-full repeat-x bg-center w-screen',
+  topLeft: 'absolute top-[1px] left-0 -translate-y-full bg-no-repeat bg-contain bg-left-top',
+  topRight: 'absolute top-[1px] right-0 -translate-y-full bg-no-repeat bg-contain bg-right-top',
   center: 'absolute top-1/2 -translate-y-1/2 repeat-x bg-center w-screen',
   centerLeft: 'absolute top-1/2 left-0 -translate-y-1/2 bg-no-repeat bg-contain bg-left-center',
   centerRight: 'absolute top-1/2 right-0 -translate-y-1/2 bg-no-repeat bg-contain bg-right-center',
-  bottom: 'absolute bottom-0 translate-y-full repeat-x bg-center w-screen',
-  bottomLeft: 'absolute bottom-0 left-0 translate-y-full bg-no-repeat bg-contain bg-left-bottom',
-  bottomRight: 'absolute bottom-0 right-0 translate-y-full bg-no-repeat bg-contain bg-right-bottom',
+  bottom: 'absolute bottom-[1px] translate-y-full repeat-x bg-center w-screen',
+  bottomLeft: 'absolute bottom-[1px] left-0 translate-y-full bg-no-repeat bg-contain bg-left-bottom',
+  bottomRight: 'absolute bottom-[1px] right-0 translate-y-full bg-no-repeat bg-contain bg-right-bottom',
 }
 
 const positionToClassIn = {
-  top: 'absolute top-0 repeat-x bg-center w-screen',
-  topLeft: 'absolute top-0 left-0 bg-no-repeat bg-contain bg-left-top',
-  topRight: 'absolute top-0 right-0 bg-no-repeat bg-contain bg-right-top',
+  top: 'absolute top-[1px] repeat-x bg-center w-screen',
+  topLeft: 'absolute top-[1px] left-0 bg-no-repeat bg-contain bg-left-top',
+  topRight: 'absolute top-[1px] right-0 bg-no-repeat bg-contain bg-right-top',
   center: 'absolute top-1/2 -translate-y-1/2 repeat-x bg-center w-screen',
   centerLeft: 'absolute top-1/2 left-0 -translate-y-1/2 bg-no-repeat bg-contain bg-left-center',
   centerRight: 'absolute top-1/2 right-0 -translate-y-1/2 bg-no-repeat bg-contain bg-right-center',
-  bottom: 'absolute bottom-0 repeat-x bg-center w-screen',
-  bottomLeft: 'absolute bottom-0 left-0 bg-no-repeat bg-contain bg-left-bottom',
-  bottomRight: 'absolute bottom-0 right-0 bg-no-repeat bg-contain bg-right-bottom'
+  bottom: 'absolute bottom-[1px] repeat-x bg-center w-screen',
+  bottomLeft: 'absolute bottom-[1px] left-0 bg-no-repeat bg-contain bg-left-bottom',
+  bottomRight: 'absolute bottom-[1px] right-0 bg-no-repeat bg-contain bg-right-bottom'
 }
 
 const glob = import.meta.glob<Record<string, string>>('@/assets/images/*', { eager: true })
