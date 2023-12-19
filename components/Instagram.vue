d530c5a314a49c7fe008fa132ce0efed34249c9a
Jonathan Schneider <jonathan@jott.media>
2023-12-19 16:57:31 +0100

    fix(instagram): link

--- components/Instagram.vue
+++ components/Instagram.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center mb-8">
-    <NuxtLink href="https://www.instgram.com/jott.media/" target="_blank" class="inline-block"><UIcon class="text-jm-primary-green text-[70px]" name="i-mdi-instagram" /></NuxtLink>
+    <NuxtLink href="https://www.instagram.com/jott.media/" target="_blank" class="inline-block"><UIcon class="text-jm-primary-green text-[70px]" name="i-mdi-instagram" /></NuxtLink>
   </div>
 
   <div class="flex justify-center gap-x-5 overflow-hidden">
@@ -25,19 +25,19 @@ const posts = ref<InstagramPosts[]>([
   {
     id: '1',
     src: '1-instagram.jpg',
-    link: 'https://www.instgram.com/jott.media/',
+    link: 'https://www.instagram.com/jott.media/',
     alt: 'Unser neues Corporate Design ist da! Wir freuen uns sehr, euch unser neues Logo präsentieren zu dürfen.',
   },
   {
     id: '2',
     src: '2-instagram.jpg',
-    link: 'https://www.instgram.com/jott.media/',
+    link: 'https://www.instagram.com/jott.media/',
     alt: 'Unser neues Corporate Design ist da! Wir freuen uns sehr, euch unser neues Logo präsentieren zu dürfen',
   },
   {
     id: '3',
     src: '3-instagram.jpg',
-    link: 'https://www.instgram.com/jott.media/',
+    link: 'https://www.instagram.com/jott.media/',
     alt: 'Unser neues Corporate Design ist da! Wir freuen uns sehr, euch unser neues Logo präsentieren zu dürfen',
   },
 ])
