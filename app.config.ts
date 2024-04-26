1c127607df7dd5df7e2d98eaba2a5986eccbf332
Jonathan Schneider <jonathan@jott.media>
2024-04-26 16:30:01 +0200

    update nuxt ui colors

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,15 @@ export default defineAppConfig({
   ui: {
     // primary: 'nuxt-ui-primary',
     // gray: 'nuxt-ui-grey'
+    variables: {
+      light: {
+        background: '255 255 255',
+        foreground: '000 000 000'
+      },
+      dark: {
+        background: '255 255 255',
+        foreground: '000 000 000'
+      },
+    }
   }
 })
