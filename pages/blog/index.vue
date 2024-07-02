6c43a42fec535d74feecbfeda7e4502dc7bd5cbf
c1chy-jott <paul@jott.media>
2024-07-02 18:01:36 +0200

    feat(blog ver.1): update blog layout and add article categories

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -8,7 +8,7 @@
             <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class="w-[336px]" :shine="false" :parallax="false" />
           </NuxtLink>
       </UContainer>
-      <UContainer class="py-10" :ui="{'constrained': 'max-w-7xl'}">
+      <UContainer class="py-8" :ui="{'constrained': 'max-w-7xl'}">
         <Headline type="h2" class="pb-12 leading-[57px] lowercase"><b class="text-jm-primary-brown uppercase">Neues</b> aus der <b class="text-jm-primary-brown uppercase"> digitalen Welt </b></Headline>
 
 
@@ -23,24 +23,34 @@
         <Button class="text-jm-primary-grey border-jm-primary-grey">Finanzen</Button>
         <Button class="text-jm-contrast-black border-jm-contrast-black">Unternehmen</Button>
         </UContainer>
-<!--        <UBlogList>
-          <UBlogPost v-for="(article, index) in articles" :key="index" v-bind="article" />
-        </UBlogList>-->
 
-        <UPagination v-model="page" :page-count="pageMaxArticles" :total="articles?.length ?? 0" :ui="{ rounded: 'first-of-type:rounded-s-md last-of-type:rounded-e-md' }">
-          <template #prev="{ onClick }">
-            <UTooltip text="Previous page">
-              <UButton icon="i-heroicons-arrow-small-left-20-solid" color="primary" :ui="{ rounded: 'rounded-full' }" class="rtl:[&_span:first-child]:rotate-180 me-2" @click="onClick" />
-            </UTooltip>
-          </template>
 
-          <template #next="{ onClick }">
-            <UTooltip text="Next page">
-              <UButton icon="i-heroicons-arrow-small-right-20-solid" color="primary" :ui="{ rounded: 'rounded-full' }" class="rtl:[&_span:last-child]:rotate-180 ms-2" @click="onClick" />
-            </UTooltip>
-          </template>
-        </UPagination>
+        <UBlogList orientation="horizontal" class="mt-16">
+          <UBlogPost v-for="(article, index) in articles" :key="index">
 
+            <NuxtImg :src="article.image" format="webp"/>
+            <Paragraph class="mb-8">{{article.date}} von <b class="text-jm-primary-green"> {{article.author}} </b></Paragraph>
+            <Headline type="h5" v-html="article.title"></Headline>
+            <Paragraph class="text-sm mb-4">{{article.description}} </Paragraph>
+            <UBadge
+                v-for="(category) in article.categories"
+                class="mr-2 py-0 text-xs text-jm-secondary-white font-bold uppercase"
+                variant="solid"
+                size="sm">{{category}}</UBadge>
+
+
+          </UBlogPost>
+        </UBlogList>
+
+
+        <!--        <UBlogPost v-for="(article, index) in articles"
+                         :description="article.description"
+                           :image="article.image"
+                           :key="index"
+                           v-bind:article="article"
+                           v-html="article.title"
+                >
+                </UBlogPost>-->
       </UContainer>
     </UPageBody>
   </UPage>
@@ -52,6 +62,12 @@ const route = useRoute()
 const page = ref(1)
 const pageMaxArticles = 10
 const { data: articles } = await useAsyncData(route.path, () => queryContent(route.path).limit(pageMaxArticles).skip(pageMaxArticles * (page.value - 1)).find())
+
+
+
+const categories = articles.value?.map(item => item.categories)
+
+console.log(categories)
 
 useHead({
   title: 'Blog - JOTT.MEDIA'
