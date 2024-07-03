8e61edb94a191b8799e5f79611085934c2331ee9
c1chy-jott <paul@jott.media>
2024-07-03 12:37:09 +0200

    feat(blog-loop): dynamic buttons categories if available and categories button classes

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -13,42 +13,37 @@
 
 
         <Paragraph class="text-base">Hier möchten wir gerne unser Wissen, über die digitale Zukunft, Technologien, Design <br> und das Leben in einer digitalen Agentur, mit euch teilen, bleibt gespannt, wir sind es auch.</Paragraph>
-
-
         <UContainer :ui="{'constrained': 'max-w-4xl space-x-4 ml-0 lg:px-0'}">
-        <Button class="text-jm-secondary-white border-transparent  bg-nuxt-ui-primary-50">Alle anzeigen</Button>
-        <Button>Design</Button>
-        <Button class="text-jm-primary-green border-jm-primary-green">Entwicklung</Button>
-        <Button class="text-jm-contrast-blue border-jm-contrast-blue">Social Media</Button>
-        <Button class="text-jm-primary-grey border-jm-primary-grey">Finanzen</Button>
-        <Button class="text-jm-contrast-black border-jm-contrast-black">Unternehmen</Button>
+          <Button v-for="(category, index) in categories" :key="index"
+                  :class="getCategoryClass(category)">{{category}}</Button>
         </UContainer>
 
-
         <UBlogList orientation="horizontal" class="mt-16">
-          <UBlogPost v-for="(article, index) in articles" :key="index">
+          <UBlogPost v-for="(article, index) in articles" :key="index" class="p-4 bg-jm-secondary-grey-lighter">
 
             <NuxtImg :src="article.image" format="webp"/>
-            <Paragraph class="mb-8">{{article.date}} von <b class="text-jm-primary-green"> {{article.author}} </b></Paragraph>
+            <Paragraph class="mt-4 mb-4">{{article.date}} von <b class="text-jm-primary-green"> {{article.author}} </b></Paragraph>
             <Headline type="h5" v-html="article.title"></Headline>
             <Paragraph class="text-sm mb-4">{{article.description}} </Paragraph>
             <UBadge
-                v-for="(category) in article.categories"
+                v-for="(category, index) in article.categories.slice(1)"
+                :key="index"
+                color="white"
                 class="mr-2 py-0 text-xs text-jm-secondary-white font-bold uppercase"
+                :class="getCategoryClass(category)"
                 variant="solid"
                 size="sm">{{category}}</UBadge>
-
-
           </UBlogPost>
         </UBlogList>
 
 
-        <!--        <UBlogPost v-for="(article, index) in articles"
+<!--                <UBlogPost v-for="(article, index) in articles"
                          :description="article.description"
                            :image="article.image"
+                           :authors="[article.author]"
+                           :date="formatDate(article.date)"
                            :key="index"
                            v-bind:article="article"
-                           v-html="article.title"
                 >
                 </UBlogPost>-->
       </UContainer>
@@ -62,12 +57,27 @@ const route = useRoute()
 const page = ref(1)
 const pageMaxArticles = 10
 const { data: articles } = await useAsyncData(route.path, () => queryContent(route.path).limit(pageMaxArticles).skip(pageMaxArticles * (page.value - 1)).find())
+const categories = articles.value?.map(item => item.categories).flat().filter((item, index, self) => self.indexOf(item) === index)
 
 
-
-const categories = articles.value?.map(item => item.categories)
-
-console.log(categories)
+function getCategoryClass(category: string): string {
+  switch (category) {
+    case 'Alle Anzeigen':
+      return 'text-jm-contrast-black text-jm-secondary-white bg-nuxt-ui-primary-50 border-nuxt-ui-primary-50';
+    case 'Design':
+      return 'text-jm-primary-brown border-jm-primary-brown';
+    case 'Entwicklung':
+      return 'text-jm-primary-green border-jm-primary-green';
+    case 'Social Media':
+      return 'text-jm-contrast-blue border-jm-contrast-blue';
+    case 'Finanzen':
+      return 'text-jm-primary-grey border-jm-primary-grey';
+    case 'Unternehmen':
+      return 'text-jm-contrast-black border-jm-contrast-black';
+    default:
+      return ''; // Default class if needed
+  }
+}
 
 useHead({
   title: 'Blog - JOTT.MEDIA'
