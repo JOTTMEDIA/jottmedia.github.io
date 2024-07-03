3e522e872fc47351c665254e6f9a97521f23206c
c1chy-jott <paul@jott.media>
2024-07-03 16:53:22 +0200

    feat(blog): update blog page layout and styling
    This commit includes changes to the layout and styling of the blog page. It includes changes to the padding, margin, font sizes, and colors. Also, removed the `getCategoryClass` function as it's no longer needed.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -8,31 +8,37 @@
             <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class="w-[336px]" :shine="false" :parallax="false" />
           </NuxtLink>
       </UContainer>
-      <UContainer class="py-8" :ui="{'constrained': 'max-w-7xl'}">
-        <Headline type="h2" class="pb-12 leading-[57px] lowercase"><b class="text-jm-primary-brown uppercase">Neues</b> aus der <b class="text-jm-primary-brown uppercase"> digitalen Welt </b></Headline>
-
-
-        <Paragraph class="text-base">Hier möchten wir gerne unser Wissen, über die digitale Zukunft, Technologien, Design <br> und das Leben in einer digitalen Agentur, mit euch teilen, bleibt gespannt, wir sind es auch.</Paragraph>
+      <UContainer class="pt-16" :ui="{'constrained': 'max-w-7xl'}">
+        <Headline type="h2" class="pb-8 leading-[57px] text-[48px] lowercase">
+          <b class="text-jm-primary-brown uppercase">Neues</b> aus der
+          <b class="text-jm-primary-brown uppercase"> digitalen Welt </b>
+        </Headline>
+        <Paragraph class="text-base mb-8">Hier möchten wir gerne unser Wissen, über die digitale Zukunft, Technologien, Design <br> und das Leben in einer digitalen Agentur, mit euch teilen, bleibt gespannt, wir sind es auch.</Paragraph>
         <UContainer :ui="{'constrained': 'max-w-4xl space-x-4 ml-0 lg:px-0'}">
-          <Button v-for="(category, index) in categories" :key="index"
-                  :class="getCategoryClass(category)">{{category}}</Button>
+          <Button
+              v-for="(category, index) in categories"
+              :key="index"
+          class="text-jm-primary-brown border-jm-primary-brown"
+          >{{category}}</Button>
         </UContainer>
 
-        <UBlogList orientation="horizontal" class="mt-16">
-          <UBlogPost v-for="(article, index) in articles" :key="index" class="p-4 bg-jm-secondary-grey-lighter">
+        <UBlogList orientation="horizontal" class="mt-12">
+          <UBlogPost v-for="(article, index) in articles" :key="index"
+                     class="bg-jm-secondary-grey-lighter ">
 
             <NuxtImg :src="article.image" format="webp"/>
-            <Paragraph class="mt-4 mb-4">{{article.date}} von <b class="text-jm-primary-green"> {{article.author}} </b></Paragraph>
-            <Headline type="h5" v-html="article.title"></Headline>
-            <Paragraph class="text-sm mb-4">{{article.description}} </Paragraph>
+            <section class="px-7 pb-5">
+            <Paragraph class="mt-4 mb-2 text-[14px] font-light">{{article.date}} von <b class="text-jm-primary-green uppercase"> {{article.author}} </b></Paragraph>
+            <Headline class="font-extrabold text-lg" type="h5" v-html="article.title"/>
+            <Paragraph class="text-sm mb-4 font-light">{{article.description}} </Paragraph>
             <UBadge
                 v-for="(category, index) in article.categories.slice(1)"
                 :key="index"
                 color="white"
-                class="mr-2 py-0 text-xs text-jm-secondary-white font-bold uppercase"
-                :class="getCategoryClass(category)"
+                class="mr-2 py-0.5 text-[12px] text-jm-secondary-white bg-jm-primary-brown font-extrabold uppercase"
                 variant="solid"
                 size="sm">{{category}}</UBadge>
+            </section>
           </UBlogPost>
         </UBlogList>
 
@@ -58,26 +64,6 @@ const page = ref(1)
 const pageMaxArticles = 10
 const { data: articles } = await useAsyncData(route.path, () => queryContent(route.path).limit(pageMaxArticles).skip(pageMaxArticles * (page.value - 1)).find())
 const categories = articles.value?.map(item => item.categories).flat().filter((item, index, self) => self.indexOf(item) === index)
-
-
-function getCategoryClass(category: string): string {
-  switch (category) {
-    case 'Alle Anzeigen':
-      return 'text-jm-contrast-black text-jm-secondary-white bg-nuxt-ui-primary-50 border-nuxt-ui-primary-50';
-    case 'Design':
-      return 'text-jm-primary-brown border-jm-primary-brown';
-    case 'Entwicklung':
-      return 'text-jm-primary-green border-jm-primary-green';
-    case 'Social Media':
-      return 'text-jm-contrast-blue border-jm-contrast-blue';
-    case 'Finanzen':
-      return 'text-jm-primary-grey border-jm-primary-grey';
-    case 'Unternehmen':
-      return 'text-jm-contrast-black border-jm-contrast-black';
-    default:
-      return ''; // Default class if needed
-  }
-}
 
 useHead({
   title: 'Blog - JOTT.MEDIA'
