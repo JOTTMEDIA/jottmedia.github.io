cef7ec62c71df3002101f2085ecf41c32a4ce5b3
c1chy-jott <paul@jott.media>
2024-07-04 19:18:22 +0200

    feat(blog-loop): update blog page layout and styling changed grid columns on breakpoints button filled color distance between posts and buttons gap

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,43 +1,48 @@
 <template>
   <UPage>
+
     <UPageBody>
-      <UContainer :ui="{'constrained': 'max-w-2xl'}">
-        <Center>
+      <UContainer :ui="{'constrained': 'max-w-7xl','base': 'text-right'}">
+
           <NuxtLink to="/" class="inline-block no-underline border-0">
-            <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class="w-[325px]" :shine="false" :parallax="false" />
+            <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class="w-[336px]" :shine="false" :parallax="false" />
           </NuxtLink>
-        </Center>
       </UContainer>
       <UContainer class="pt-16" :ui="{'constrained': 'max-w-7xl'}">
-        <Headline type="h2" class="pb-8 leading-[57px] text-[48px] lowercase">
+        <Headline type="h2" class="pb-8 leading-[57px] text-5xl lowercase">
           <b class="text-jm-primary-brown uppercase">Neues</b> aus der
           <b class="text-jm-primary-brown uppercase"> digitalen Welt </b>
         </Headline>
         <Paragraph class="text-base mb-8">Hier möchten wir gerne unser Wissen, über die digitale Zukunft, Technologien, Design <br> und das Leben in einer digitalen Agentur, mit euch teilen, bleibt gespannt, wir sind es auch.</Paragraph>
-        <UContainer :ui="{'constrained': 'max-w-4xl space-x-4 ml-0 lg:px-0'}">
+        <UContainer :ui="{
+          constrained: 'max-w-4xl space-x-4 ml-0',
+          padding: 'px-0 sm:px-0 lg:px-0'
+        }">
           <Button
               v-for="(category, index) in categories"
               :key="index"
               @click="selectedCategory = category"
-          class="text-jm-primary-brown border-jm-primary-brown"
-          >{{category}}</Button>
+              class="text-jm-primary-brown border-jm-primary-brown"
+              :class="index === 0 ? 'bg-jm-primary-brown text-jm-secondary-white' : 'defaultClass'"
+               >{{category}}</Button>
         </UContainer>
 
-        <UBlogList orientation="horizontal" class="mt-12">
+        <UBlogList orientation="horizontal" class="mt-12 gap-y-8 lg:grid-cols-2 xl:grid-cols-3">
+
           <UBlogPost
               v-for="(article, index) in filteredArticles" :key="index"
-                     class="bg-jm-secondary-grey-lighter ">
+                     class="bg-jm-secondary-grey-lighter">
             <NuxtLink :to="article._path">
-            <NuxtImg :src="article.image" format="webp"/>
+            <NuxtImg class="w-full" :src="article.image" format="webp"/>
             <section class="px-7 pb-5">
-            <Paragraph class="mt-4 mb-2 text-[14px] font-light">{{article.date}} von <b class="text-jm-primary-green uppercase"> {{article.author}} </b></Paragraph>
+            <Paragraph class="mt-4 mb-2 text-sm font-light">{{article.date}} von <b class="text-jm-primary-green uppercase"> {{article.author}} </b></Paragraph>
             <Headline class="font-extrabold text-lg" type="h5" v-html="article.title"/>
             <Paragraph class="text-sm mb-4 font-light">{{ truncateText(article.description, 250) }}  </Paragraph>
             <UBadge
                 v-for="(category, index) in article.categories.slice(1)"
                 :key="index"
                 color="white"
-                class="mr-2 py-0.5 text-[12px] text-jm-secondary-white bg-jm-primary-brown font-extrabold uppercase"
+                class="mr-2 py-0.5 text-xs text-jm-secondary-white bg-jm-primary-brown font-extrabold uppercase"
                 variant="solid"
                 size="sm">{{category}}</UBadge>
             </section>
@@ -57,7 +62,7 @@ const pageMaxArticles = 10
 const { data: articles } = await useAsyncData(route.path, () => queryContent(route.path).limit(pageMaxArticles).skip(pageMaxArticles * (page.value - 1)).find())
 const categories = articles.value?.map(item => item.categories).flat().filter((item, index, self) => self.indexOf(item) === index)
 
-const selectedCategory = ref(null);
+const selectedCategory = ref(null)
 const filteredArticles = computed(() => {
   if (!selectedCategory.value) return articles.value;
   return articles.value?.filter(article => article.categories.includes(selectedCategory.value));
