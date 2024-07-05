8e42998db4ae0c1dfc7b1ad58d3251ba5ce8c114
Jonathan Schneider <jonathan@jott.media>
2024-07-05 14:16:53 +0200

    feat: update blog page layout and content display
    This commit updates the layout of the blog page in the Jottmedia project. It improves how blog posts are displayed, with modifications to the positioning and styling of elements such as the logo, headlines, paragraphs, and buttons. It also refactors the script to fetch categories and truncate text for more efficient and cleaner code.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,18 +2,22 @@
   <UPage>
 
     <UPageBody>
-      <UContainer :ui="{'constrained': 'max-w-7xl','base': 'text-right'}">
-
-          <NuxtLink to="/" class="hidden lg:inline-block no-underline border-0">
-            <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class=" lg:w-[336px]" :shine="false" :parallax="false" />
+      <UContainer :ui="{'constrained': 'max-w-2xl'}">
+        <Center>
+          <NuxtLink to="/" class="inline-block no-underline border-0">
+            <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class="w-[325px]" :shine="false" :parallax="false"/>
           </NuxtLink>
+        </Center>
       </UContainer>
       <UContainer class="pt-16" :ui="{'constrained': 'max-w-7xl'}">
         <Headline type="h2" class="pb-8 leading-8 lg:leading-5 text-3xl lowercase">
           <b class="text-jm-primary-brown uppercase">Neues</b> aus der
           <b class="text-jm-primary-brown uppercase"> digitalen Welt </b>
         </Headline>
-        <Paragraph class="text-sm lg:text-tiny mb-8">Hier möchten wir gerne unser Wissen, über die digitale Zukunft, Technologien, Design <br> und das Leben in einer digitalen Agentur, mit euch teilen, bleibt gespannt, wir sind es auch.</Paragraph>
+        <Paragraph class="text-sm lg:text-tiny mb-8">Hier möchten wir gerne unser Wissen, über die digitale Zukunft,
+          Technologien, Design <br> und das Leben in einer digitalen Agentur, mit euch teilen, bleibt gespannt, wir sind
+          es auch.
+        </Paragraph>
         <UContainer :ui="{
           constrained: 'max-w-4xl space-x-4 space-y-4 ml-0 ',
           padding: 'px-0 sm:px-0 lg:px-0'
@@ -24,28 +28,31 @@
               @click="selectedCategory = category"
               class="text-jm-primary-brown border-jm-primary-brown "
               :class="index === 0 ? 'bg-jm-primary-brown text-jm-secondary-white' : 'defaultClass'"
-               >{{category}}</Button>
+          >{{ category }}
+          </Button>
         </UContainer>
 
         <UBlogList orientation="horizontal" class="mt-10 gap-y-8 lg:grid-cols-2 xl:grid-cols-3">
 
           <UBlogPost
               v-for="(article, index) in filteredArticles" :key="index"
-                     class="bg-jm-secondary-grey-lighter">
+              class="bg-jm-secondary-grey-lighter">
             <NuxtLink :to="article._path">
-            <NuxtImg class="w-full" :src="article.image" format="webp"/>
-            <section class="px-3 lg:px-7 pb-5">
-            <Paragraph class="mt-4 mb-2 text-sm font-light">{{article.date}} von <b class="text-jm-primary-green uppercase"> {{article.author}} </b></Paragraph>
-            <Headline class="font-extrabold text-lg" type="h5" v-html="article.title"/>
-            <Paragraph class="text-sm mb-4 font-light">{{ truncateText(article.description, 250) }}  </Paragraph>
-            <UBadge
-                v-for="(category, index) in article.categories.slice(1)"
-                :key="index"
-                color="white"
-                class="mr-2 py-0.5 text-xs text-jm-secondary-white bg-jm-primary-brown font-extrabold uppercase"
-                variant="solid"
-                size="sm">{{category}}</UBadge>
-            </section>
+              <NuxtImg class="w-full" :src="article.image" format="webp"/>
+              <section class="px-3 lg:px-7 pb-5">
+                <Paragraph class="mt-4 mb-2 text-sm font-light">{{ article.date }} von <b
+                    class="text-jm-primary-green uppercase"> {{ article.author }} </b></Paragraph>
+                <Headline class="font-extrabold text-lg" type="h5" v-html="article.title"/>
+                <Paragraph class="text-sm mb-4 font-light">{{ truncateText(article.description, 250) }}</Paragraph>
+                <UBadge
+                    v-for="(category, index) in article.categories.slice(1)"
+                    :key="index"
+                    color="white"
+                    class="mr-2 py-0.5 text-xs text-jm-secondary-white bg-jm-primary-brown font-extrabold uppercase"
+                    variant="solid"
+                    size="sm">{{ category }}
+                </UBadge>
+              </section>
             </NuxtLink>
           </UBlogPost>
         </UBlogList>
@@ -53,33 +60,32 @@
         <Button
             @click="loadMorePosts"
             class="mt-8 mx-auto flex text-jm-primary-brown border-jm-primary-brown"
-        >Mehr Anzeigen</Button>
+        >Mehr Anzeigen
+        </Button>
       </UContainer>
     </UPageBody>
   </UPage>
 </template>
 
 <script setup lang="ts">
-
 const route = useRoute()
 const page = ref(1)
 const categories: Ref<string[] | undefined> = ref([])
 
 const pageMaxArticles = ref(6)
-const { data: articles } = await useAsyncData(route.path, () =>
-    queryContent(route.path)
-        .limit(pageMaxArticles.value)
-        .skip(pageMaxArticles.value * (page.value - 1))
-        .find())
-
+const {data: articles} = await useAsyncData(route.path, () =>
+  queryContent(route.path)
+    .limit(pageMaxArticles.value)
+    .skip(pageMaxArticles.value * (page.value - 1))
+    .find())
 
 
 function fetchCategories() {
-
   categories.value = articles.value?.map(item => item.categories)
-      .flat()
-      .filter((item, index, self) => self.indexOf(item) === index)
+    .flat()
+    .filter((item, index, self) => self.indexOf(item) === index)
 }
+
 const selectedCategory = ref('')
 const filteredArticles = computed(() => {
   if (!selectedCategory.value) return articles.value;
@@ -91,12 +97,13 @@ async function loadMorePosts() {
   await useAsyncData(route.path, () =>
       queryContent(route.path)
           .limit(pageMaxArticles.value)
-          .find())
+          .find()
+  )
   fetchCategories()
 }
 
 
-function truncateText(text: string, maxLength:number) {
+function truncateText(text: string, maxLength: number) {
   if (text.length > maxLength) {
     return text.substring(0, maxLength) + '...';
   }
