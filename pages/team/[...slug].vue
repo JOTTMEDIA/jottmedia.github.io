e5c39aa16caa1df31a0aa1f2a874228d3b33bd73
c1chy-jott <paul@jott.media>
2024-07-09 18:22:55 +0200

    feat(personen-overlay): refactor team page for improved structure and readability

--- pages/team/[...slug].vue
+++ pages/team/[...slug].vue
@@ -1,18 +1,14 @@
 <template>
 
 
-
-      <UContainer class="flex bg-jm-primary-green" :ui="{'constrained': 'max-w-full gap-0 lg:px-0 h-screen'}" v-for="person in data">
-
-          <div class="w-1/2">
-
-          <Image class="w-full h-full" :src="person.image"/>
-</div>
-        <div class="w-1/2 flex flex-col justify-center px-16">
+<section>
+      <UContainer class="flex bg-jm-primary-green" :ui="{'constrained': 'max-w-full gap-0 lg:px-0 h-screen'}" v-for="person in team">
+        <div class="w-1/2">
+            <NuxtImg class="w-full h-full" :src="person.image"/>
+</div><div class="w-1/2 flex flex-col justify-center px-16">
           <h1 v-html="person?.name" class=" py-16 text-xl uppercase font-extrabold" />
           <p v-html="person?.description" class="text-sm w-3/4"></p>
-
-        <UContainer class="relative pt-10 z-10">
+          <UContainer class="relative pt-10 z-10">
           <Center>
             <NuxtLink href="https://calendar.app.google/rBDjAnPNYEQpfMvJ9" target="_blank">
               <Button>Lerne uns kennen</Button>
@@ -21,16 +17,14 @@
         </UContainer>
         </div>
       </UContainer>
+</section>
 </template>
 
 <script setup lang="ts">
 const route = useRoute()
-console.log(route)
-
-const pageMaxArticles = 1
-const { data  } = await useAsyncData(route.path, () => queryContent('team').limit(pageMaxArticles).find())
-console.log(data)
-
-
-
+const pageMaxPersons = 1
+const {data: team} = await useAsyncData(route.path, () =>
+    queryContent(route.path)
+        .limit(pageMaxPersons)
+        .find())
 </script>
