16931f1fe79d4e9ac2566ccb6de07b1dabc85982
c1chy-jott <paul@jott.media>
2024-07-09 18:58:21 +0200

    feat: update team page layout and styling backgrounds and buttons colors

--- pages/team/[...slug].vue
+++ pages/team/[...slug].vue
@@ -1,23 +1,26 @@
 <template>
-
-
-<section>
-      <UContainer class="flex bg-jm-primary-green" :ui="{'constrained': 'max-w-full gap-0 lg:px-0 h-screen'}" v-for="person in team">
+  <UPage>
+    <UPageBody class="mt-0 pb-0">
+      <UContainer
+          class="flex"
+          :class="person.bg"
+          :ui="{'constrained': 'max-w-full gap-0 lg:px-0 h-screen'}" v-for="person in team">
         <div class="w-1/2">
             <NuxtImg class="w-full h-full" :src="person.image"/>
-</div><div class="w-1/2 flex flex-col justify-center px-16">
+</div> <div class="w-1/2 flex flex-col justify-center px-24">
           <h1 v-html="person?.name" class=" py-16 text-xl uppercase font-extrabold" />
-          <p v-html="person?.description" class="text-sm w-3/4"></p>
+          <p v-html="person?.description" class="text-sm "></p>
           <UContainer class="relative pt-10 z-10">
           <Center>
             <NuxtLink href="https://calendar.app.google/rBDjAnPNYEQpfMvJ9" target="_blank">
-              <Button>Lerne uns kennen</Button>
+              <Button :class="person.button">Lerne uns kennen</Button>
             </NuxtLink>
           </Center>
         </UContainer>
         </div>
       </UContainer>
-</section>
+    </UPageBody>
+  </UPage>
 </template>
 
 <script setup lang="ts">
