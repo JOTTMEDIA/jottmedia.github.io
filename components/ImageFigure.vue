3497fe1b84634cd46d14c149ca80735115c5424d
c1chy-jott <paul@jott.media>
2024-07-10 17:43:21 +0200

    feat: update ImageFigure component and index page
    - Update the ImageFigure component to include a new quote prop and a button.
    - Refactor the index page to use the updated ImageFigure component with the new props.
    - Replace hard-coded team member data with dynamic data fetched from the "team" content type.
    - Persons md files upgrade

--- components/ImageFigure.vue
+++ components/ImageFigure.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="flex" :class="typeToClass[align]">
     <div class="md:w-2/5 space-y-10 relative">
-
-      <NuxtLink :to="to" >
-      <Image :src="src" :alt="hint ?? ''" />
+      <Image :src="src" :alt="hint ?? ''"/>
+      <NuxtLink :to="link">
+        <button
+            class="w-12 h-12  bg-jm-contrast-black bg-opacity-70 hover:bg-opacity-100 flex justify-center items-center rounded-full absolute -right-6 -bottom-5  transition-all z-10">
+          <UIcon name="i-mdi-plus" class="text-2xl h-9 w-9 text-jm-primary-brown"/>
+        </button>
+      </NuxtLink>
       <div class="md:absolute">
-        <Headline type="h5"><slot /></Headline>
+        <h5 class="text-lg uppercase h-animation-bigger my-0 mb-4" v-html="quote">
+        </h5>
         <Paragraph v-if="hint != null" class="italic text-sm mb-10">{{ hint }}</Paragraph>
       </div>
-      </NuxtLink>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 
-const props = defineProps({
+
+defineProps({
   align: {
     type: String,
     default: 'left'
@@ -28,7 +33,11 @@ const props = defineProps({
     type: String,
     default: null
   },
-  to: {
+  quote: {
+    type: String,
+    default: null
+  },
+  link: {
     type: String,
     default: null
   }
--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,18 @@
     <UPageBody class="m-0 p-0">
       <div class="relative bg-jm-secondary-grey-lighter">
 
-        <UContainer class="relative py-10 flex h-screen flex-col justify-between gap-y-5 z-10" :ui="{'constrained': 'max-w-3xl'}">
+        <UContainer class="relative py-10 flex h-screen flex-col justify-between gap-y-5 z-10"
+                    :ui="{'constrained': 'max-w-3xl'}">
           <Center>
             <NuxtLink to="/" class="inline-block no-underline border-0">
-              <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class="w-[325px]" :shine="false" :parallax="false" />
+              <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class="w-[325px]" :shine="false" :parallax="false"/>
             </NuxtLink>
           </Center>
 
           <Center>
-            <Headline type="h1" class="pb-5">Machen wir es<br><span class="text-jm-primary-green uppercase">einfach:</span> <b class="uppercase">digital</b>.</Headline>
+            <Headline type="h1" class="pb-5">Machen wir es<br><span
+                class="text-jm-primary-green uppercase">einfach:</span> <b class="uppercase">digital</b>.
+            </Headline>
             <NuxtLink :to="{ path: '/', hash: '#machen' }">
               <Button>Einfach machen</Button>
             </NuxtLink>
@@ -19,34 +22,44 @@
           <Center>
             <Headline type="h3">Dein Büro <span class="lowercase">für</span> <b>Entwicklung und Design.</b></Headline>
             <NuxtLink :to="{ path: '/', hash: '#machen' }">
-              <UIcon class="text-jm-secondary-grey text-[70px]" name="i-mdi-arrow-down" />
+              <UIcon class="text-jm-secondary-grey text-[70px]" name="i-mdi-arrow-down"/>
             </NuxtLink>
           </Center>
         </UContainer>
-        <Background width="1100px" height="1100px" src="header-box.png" position="bottomLeft" :out="false" />
-        <Background height="701px" src="header-grey.svg" position="bottom" :out="false" parallax="to-right" />
+        <Background width="1100px" height="1100px" src="header-box.png" position="bottomLeft" :out="false"/>
+        <Background height="701px" src="header-grey.svg" position="bottom" :out="false" parallax="to-right"/>
       </div>
       <div class="relative bg-jm-primary-green">
-        <Background height="644px" src="header-green-top.svg" position="top" parallax="to-left" />
+        <Background height="644px" src="header-green-top.svg" position="top" parallax="to-left"/>
         <UContainer id="machen" class="relative py-10" :ui="{'constrained': 'max-w-3xl'}">
           <Headline type="h6">Das bekommst du <b class="uppercase">von uns</b></Headline>
-          <Headline type="h4"><b>Mehr Zeit</b>, sodass du deine Energie auf den <b>Erfolg</b> und das Wachstum deines Unternehmens fokussieren kannst. Dazu vereinfachen und digitalisieren wir deine Prozesse – und sorgen so für <b>mehr Effizienz, Zufriedenheit und Qualität</b> für dein Business.</Headline>
+          <Headline type="h4"><b>Mehr Zeit</b>, sodass du deine Energie auf den <b>Erfolg</b> und das Wachstum deines
+            Unternehmens fokussieren kannst. Dazu vereinfachen und digitalisieren wir deine Prozesse – und sorgen so für
+            <b>mehr Effizienz, Zufriedenheit und Qualität</b> für dein Business.
+          </Headline>
         </UContainer>
-        <Background height="875px" src="header-green-bottom.svg" position="bottom" parallax="to-left" />
+        <Background height="875px" src="header-green-bottom.svg" position="bottom" parallax="to-left"/>
       </div>
       <UContainer class="relative py-10" :ui="{'constrained': 'max-w-4xl'}">
-        <Image src="team.jpg" alt="Arian und Jan im Termin" />
+        <Image src="team.jpg" alt="Arian und Jan im Termin"/>
       </UContainer>
 
       <UContainer class="relative py-10" :ui="{'constrained': 'max-w-2xl'}">
         <Headline type="h6">Die wichtigste <b class="uppercase">Frage</b></Headline>
-        <Headline type="h2" class="font-[400]">„<b class="uppercase">Was</b> genau <b class="uppercase">braucht dein Unternehmen wirklich?</b>“</Headline>
+        <Headline type="h2" class="font-[400]">„<b class="uppercase">Was</b> genau <b class="uppercase">braucht dein
+          Unternehmen wirklich?</b>“
+        </Headline>
       </UContainer>
 
       <UContainer class="relative py-10" :ui="{'constrained': 'max-w-2xl'}">
         <Headline type="h6">So arbeiten wir <b class="uppercase">für Dich</b></Headline>
         <Headline type="h5" class="text-jm-primary-brown"><b>Ehrlich. zuverlässig. partnerschaftlich.</b></Headline>
-        <Paragraph>Die Lösung muss immer zum Problem passen. Darum steht bei uns <b>an erster Stelle</b> die Analyse deiner Herausforderung. Erst, wenn diese glasklar ist, widmen wir uns der <b>Konzeption und Umsetzung</b> des tatsächlichen Produkts, wie auch immer das in deinem konkreten Fall aussehen mag. Damit das funktioniert, ist eine ehrliche, zuverlässige und partnerschaftliche Zusammenarbeit – <b>auf Augenhöhe</b> und auf beiden Seiten – unabdingbar.</Paragraph>
+        <Paragraph>Die Lösung muss immer zum Problem passen. Darum steht bei uns <b>an erster Stelle</b> die Analyse
+          deiner Herausforderung. Erst, wenn diese glasklar ist, widmen wir uns der <b>Konzeption und Umsetzung</b> des
+          tatsächlichen Produkts, wie auch immer das in deinem konkreten Fall aussehen mag. Damit das funktioniert, ist
+          eine ehrliche, zuverlässige und partnerschaftliche Zusammenarbeit – <b>auf Augenhöhe</b> und auf beiden Seiten
+          – unabdingbar.
+        </Paragraph>
         <Center>
           <NuxtLink href="https://calendar.app.google/rBDjAnPNYEQpfMvJ9" target="_blank">
             <Button>Lass uns gemeinsam loslegen</Button>
@@ -54,17 +67,20 @@
         </Center>
       </UContainer>
       <div class="relative">
-        <Background height="757px" src="grey-top.svg" position="top" :out="false" parallax="to-right" />
+        <Background height="757px" src="grey-top.svg" position="top" :out="false" parallax="to-right"/>
         <UContainer class="relative py-10 md:mb-40 z-10" :ui="{'constrained': 'max-w-4xl'}">
-          <ImageFigure to="/team/annika"  src="annika.jpg" hint="Annika // Design" align="left">„<b class="text-jm-primary-brown">Schon als Kind</b> war ich immer <b>fasziniert</b> von farben, formen und mustern und habe es geliebt, <b>neue Dinge zu erschaffen.</b>“</ImageFigure>
-            <ImageFigure to="/team/jan"  src="jan.jpg" hint="Jan // Design" align="right">„<b class="text-jm-primary-brown">Mein Mut zur Veränderung</b> und somit unsere Kompetenzen ergänzend nutzen zu können, <b>war die beste Entscheidung</b> meines Lebens.“</ImageFigure>
-           <ImageFigure to="/team/arian" src="arian.jpg" hint="Arian // Entwicklung" align="left">„<b class="text-jm-primary-brown">Es gibt</b> keine bessere Schönheit als <b>den Intellekt.</b>“</ImageFigure>
-          <ImageFigure  to="/team/jonathan" src="jonathan.jpg" hint="Jonathan // Entwicklung" align="right">„<b class="text-jm-primary-brown">Mich begeistert</b> die unendliche Kreativität, mit meinen Händen und meinem MacBook <b>die Welt zu bereichern.</b>“</ImageFigure>
-                  <ImageFigure  to="/team/inke"  src="inke.jpg" hint="Inke // Finanzen" align="left">„<b class="text-jm-primary-brown">Obwohl</b> in meiner Schulzeit zuerst die <b>Mengenlehre</b> unterrichtet wurde, <b>sind</b> die <b>Zahlen mein</b> ständiger <b>Begleiter</b> geworden.“</ImageFigure>
+          <ImageFigure
+              v-for="person in team"
+              :to="person.permaLink"
+              :src="person.image"
+              :quote="person.quote"
+              :hint="person.hint"
+              :link="person.link"
+              :align="person.align">
+          </ImageFigure>
         </UContainer>
-        <Background height="854px" src="grey-bottom.svg" position="bottom" :out="false" parallax="to-left" />
+        <Background height="854px" src="grey-bottom.svg" position="bottom" :out="false" parallax="to-left"/>
       </div>
-
       <UContainer class="relative py-10 z-10" :ui="{'constrained': 'max-w-2xl'}">
         <Center>
           <NuxtLink href="https://calendar.app.google/rBDjAnPNYEQpfMvJ9" target="_blank">
@@ -74,20 +90,26 @@
       </UContainer>
 
       <div class="relative bg-jm-primary-green mt-64">
-        <Background height="702px" src="green-top.svg" position="top" parallax="to-right" />
+        <Background height="702px" src="green-top.svg" position="top" parallax="to-right"/>
         <UContainer class="relative py-10" :ui="{'constrained': 'max-w-2xl'}">
           <Headline type="h6">Das leisten wir <b class="uppercase">für dich</b></Headline>
-          <Headline type="h3" class="font-[400]">Wir entwickeln und designen <b class="text-jm-primary-brown">digitale Premiumlösungen</b> <b>individuell für dein Unternehmen.</b></Headline>
+          <Headline type="h3" class="font-[400]">Wir entwickeln und designen <b class="text-jm-primary-brown">digitale
+            Premiumlösungen</b> <b>individuell für dein Unternehmen.</b></Headline>
         </UContainer>
-        <Background height="517px" src="green-bottom.svg" position="bottom" parallax="to-right" />
+        <Background height="517px" src="green-bottom.svg" position="bottom" parallax="to-right"/>
       </div>
 
       <UContainer class="py-10" :ui="{'constrained': 'max-w-4xl'}">
-        <Carousel :items="carouselItems" />
+        <Carousel :items="carouselItems"/>
       </UContainer>
 
       <UContainer class="py-10" :ui="{'constrained': 'max-w-2xl'}">
-        <Paragraph><b class="text-jm-primary-brown uppercase">Konzeption, Prototyping, Programmierung & Design</b> von <b>digitalen Lösungen</b>, mit denen du deine Herausforderungen optimal <b>meisterst</b>. Anders gesagt: <b>wir machen Kompliziertes einfach</b> – und räumen Wachstumsbremsen aus dem Weg. <b>Ganz gleich, ob Webseite, Web-Applikation oder App</b>: Unsere Lösungen zeichnen sich durch beeindruckende Funktionalität, begeisternde Userfreundlichkeit und <b>bestechende Ästhetik</b> aus. Hand drauf.</Paragraph>
+        <Paragraph><b class="text-jm-primary-brown uppercase">Konzeption, Prototyping, Programmierung & Design</b> von
+          <b>digitalen Lösungen</b>, mit denen du deine Herausforderungen optimal <b>meisterst</b>. Anders gesagt: <b>wir
+            machen Kompliziertes einfach</b> – und räumen Wachstumsbremsen aus dem Weg. <b>Ganz gleich, ob Webseite,
+            Web-Applikation oder App</b>: Unsere Lösungen zeichnen sich durch beeindruckende Funktionalität,
+          begeisternde Userfreundlichkeit und <b>bestechende Ästhetik</b> aus. Hand drauf.
+        </Paragraph>
         <Center>
           <NuxtLink href="https://calendar.app.google/rBDjAnPNYEQpfMvJ9" target="_blank">
             <Button>Los geht's</Button>
@@ -101,6 +123,11 @@
 useHead({
   title: 'Dein Büro für Entwicklung und Design – JOTT.MEDIA'
 })
+const route = useRoute()
+
+const {data: team} = await useAsyncData(route.path, () =>
+    queryContent("team").find())
+
 
 const carouselItems = ref([
   {
