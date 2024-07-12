0e604b4c817386c657d06c817042283be8ee97ab
c1chy-jott <paul@jott.media>
2024-07-12 17:07:28 +0200

    feat(team page): update UI layout and styles for better responsiveness
    This commit includes changes to the team page layout and styles to improve responsiveness. It includes adjustments to flex properties, padding, text sizes, and image elements. The UI changes aim to provide a better user experience across different screen sizes.

--- pages/team/[...slug].vue
+++ pages/team/[...slug].vue
@@ -1,24 +1,24 @@
 <template>
   <UPage>
-    <UPageBody class="mt-0 pb-0">
+    <UPageBody class="mt-0 pb-0 h-screen">
       <UContainer
           v-for="person in team"
-          class="flex"
-          :class="person.bg"
-          :ui="{'constrained': 'max-w-full gap-0 lg:px-0 h-screen'}">
-        <div class="w-1/2">
-          <NuxtImg class="w-full h-full" :src="person.image"/>
+          class="flex flex-col sm:flex-row h-screen "
+          :ui="{'constrained': 'max-w-full gap-0 px-0 sm:px-0 lg:px-0 '}">
+        <div class="sm:w-1/2">
+          <Image class="w-full h-full" :src="person.src"/>
         </div>
-        <div class="w-1/2 flex flex-col justify-center px-24">
-          <button class="text-black w-12 h-12 relative focus:outline-none bg-transparent z-[100] self-end" @click="navigateBack">
+        <div class="sm:w-1/2 flex flex-col justify-between px-4 lg:py-8 xl:py-4 xl:px-8 2xl:py-16 2xl:px-20" :class="person.bg">
+          <button class="text-black w-12 h-12 relative focus:outline-none bg-transparent z-[100] self-end"
+                  @click="navigateBack">
             <UIcon name="i-mdi-close-thick" class="text-2xl"/>
           </button>
-          <h1 v-html="person?.name" class=" py-16 text-xl uppercase font-extrabold"/>
-          <p v-html="person?.description" class="text-sm "></p>
-          <UContainer class="relative pt-10 z-10">
+          <h1 v-html="person?.name" class=" sm:leading-5 xl:leading-10 xl:text-xl uppercase font-extrabold "/>
+          <p v-html="person?.description" class="text-xs xl:text-sm"></p>
+          <UContainer class="relative pt-0 z-10">
             <Center>
               <NuxtLink href="https://calendar.app.google/rBDjAnPNYEQpfMvJ9" target="_blank">
-                <Button :class="person.button">Lerne uns kennen</Button>
+                <Button class=" text-xs lg:text-base my-4" :class="person.button">Lerne uns kennen</Button>
               </NuxtLink>
             </Center>
           </UContainer>
@@ -36,9 +36,11 @@ const route = useRoute()
 const router = useRouter()
 
 const pageMaxPersons = 1
+
 function navigateBack() {
   router.back();
 }
+
 const {data: team} = await useAsyncData(route.path, () =>
     queryContent(route.path)
         .limit(pageMaxPersons)
