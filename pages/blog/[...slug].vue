811463dd6bf7df17202f9e14c530366da13832f1
c1chy-jott <paul@jott.media>
2024-07-19 14:36:04 +0200

    feat: improve code readability in blog page component
    Refactored the blog page component to improve code readability. Added line breaks and indentation to make the code more understandable.

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,28 +1,31 @@
 <template>
   <UPage>
-    <UPageBody prose class="prose-h1:normal-case prose-h1:font-normal prose-headings:uppercase prose-lead:uppercase prose-lead:text-base">
+    <UPageBody prose
+               class="prose-h1:normal-case prose-h1:font-normal prose-headings:uppercase prose-lead:uppercase prose-lead:text-base">
       <UContainer :ui="{'constrained': 'max-w-2xl'}">
         <Center>
           <NuxtLink to="/" class="inline-block no-underline border-0">
-            <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class="w-[325px] mt-2" :shine="false" :parallax="false" />
+            <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class="w-[325px] mt-2" :shine="false" :parallax="false"/>
           </NuxtLink>
         </Center>
       </UContainer>
       <UContainer class="pt-10" :ui="{'constrained': 'max-w-2xl'}">
-        <h1 v-html="page?.title" class="text-4xl" />
+        <h1 v-html="page?.title" class="text-4xl"/>
         <div class="flex gap-2 mb-2">
-          <UBadge :ui="{ rounded: 'rounded-lg', font: 'font-extrabold', size: { xs: 'text-xs px-3 py-0.5' } }" v-for="category in page?.categories" :key="category" :label="category" size="xs" />
+          <UBadge :ui="{ rounded: 'rounded-lg', font: 'font-extrabold', size: { xs: 'text-xs px-3 py-0.5' } }"
+                  v-for="category in page?.categories" :key="category" :label="category" size="xs"/>
         </div>
-        <NuxtLink  :to="`../team/${page?.author.toLowerCase()}`">
-          <small>{{ page?.date }} von <b class="text-jm-primary-green">{{ page?.author }}
-          </b></small></NuxtLink>
+        <small>{{ page?.date }} von
+          <NuxtLink :to="`../team/${page?.author.toLowerCase()}`"><b class="text-jm-primary-green">{{ page?.author }}
+          </b></NuxtLink>
+        </small>
         <p class="lead">{{ page?.description }}</p>
       </UContainer>
       <UContainer :ui="{'constrained': 'max-w-4xl'}">
-        <Image :src="page?.image" :public-src="true" :alt="page?.imageAlt" :hint="page?.imageAlt" />
+        <Image :src="page?.image" :public-src="true" :alt="page?.imageAlt" :hint="page?.imageAlt"/>
       </UContainer>
       <UContainer class="pb-10" :ui="{'constrained': 'max-w-2xl'}">
-        <ContentRenderer v-if="page?.body" :value="page" />
+        <ContentRenderer v-if="page?.body" :value="page"/>
       </UContainer>
     </UPageBody>
   </UPage>
@@ -31,7 +34,7 @@
 <script setup lang="ts">
 
 const route = useRoute()
-const { data: page } = await useAsyncData(route.path, () => queryContent(route.path).findOne())
+const {data: page} = await useAsyncData(route.path, () => queryContent(route.path).findOne())
 
 useSeoMeta({
   title: page.value?.seoTitle + ' - Blog - JOTT.MEDIA',
