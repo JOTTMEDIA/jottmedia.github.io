b906ddfb969aac33ade90bbd923f33a0e9d92b87
c1chy-jott <paul@jott.media>
2024-08-05 18:08:49 +0200

    feat(nuxt.config): optimize webpack settings and add vite compression plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,89 +1,123 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import viteCompression from 'vite-plugin-compression'
+
 export default defineNuxtConfig({
-  devtools: {
-    enabled: true,
-    timeline: {
-      enabled: true
-    }
-  },
-
-  routeRules: {
-    '/': { prerender: true },
-  },
-
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
-    head: {
-      title: 'Dein Büro für Entwicklung und Design – JOTT.MEDIA',
-      htmlAttrs: {
-        lang: 'de',
-      },
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Wir unterstützen dich und dein Unternehmen eigene Prozesse digital zu vereinfachen, um mehr Zeit, mehr Zufriedenheit und mehr Qualität zu gewinnen und erfoglreich zu wachsen.' },
-        { name: 'format-detection', content: 'telephone=no' },
-        { name: 'msapplication-TileColor', content: '#ffffff' },
-        { name: 'theme-color', content: '#ffffff' },
-      ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        {
-          rel: 'apple-touch-icon',
-          sizes: '180x180',
-          href: '/apple-touch-icon.png',
+    webpack: {
+        extractCSS: true,
+        optimization: {
+            minimize: true,
+            splitChunks: {
+                chunks: 'all',
+            },
         },
-        {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '32x32',
-          href: '/favicon-32x32.png',
-        },
-        {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '16x16',
-          href: '/favicon-16x16.png',
-        },
-        { rel: 'manifest', href: '/site.webmanifest' },
-        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
-      ]
     },
-  },
+    postcss: {
+        plugins: {
+            autoprefixer: {},
+            cssnano: {preset: 'default'}
+        },
+    },
+    experimental: {
+        payloadExtraction: false,
+        renderJsonPayloads: false,
+    },
 
-  extends: ['@nuxt/ui-pro'],
+    devtools: {
+        enabled: true,
+        timeline: {
+            enabled: true
+        }
+    },
+    routeRules: {
+        '/': {prerender: true},
+    },
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/ui',
-    '@vueuse/nuxt',
-    '@nuxt/image',
-    '@nuxthq/studio',
-    '@nuxtjs/web-vitals',
-    "@nuxtjs/fontaine"
-  ],
+    app: {
+        pageTransition: {name: 'page', mode: 'out-in'},
+        head: {
+            title: 'Dein Büro für Entwicklung und Design – JOTT.MEDIA',
+            htmlAttrs: {
+                lang: 'de',
+            },
+            meta: [
+                {charset: 'utf-8'},
+                {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+                {
+                    name: 'description',
+                    content: 'Wir unterstützen dich und dein Unternehmen eigene Prozesse digital zu vereinfachen, um mehr Zeit, mehr Zufriedenheit und mehr Qualität zu gewinnen und erfoglreich zu wachsen.'
+                },
+                {name: 'format-detection', content: 'telephone=no'},
+                {name: 'msapplication-TileColor', content: '#ffffff'},
+                {name: 'theme-color', content: '#ffffff'},
+            ],
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+                {
+                    rel: 'apple-touch-icon',
+                    sizes: '180x180',
+                    href: '/apple-touch-icon.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '32x32',
+                    href: '/favicon-32x32.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '16x16',
+                    href: '/favicon-16x16.png',
+                },
+                {rel: 'manifest', href: '/site.webmanifest'},
+                {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
+            ]
+        },
+    },
 
-  pages: true,
+    extends: ['@nuxt/ui-pro'],
 
-  colorMode: {
-    preference: 'light'
-  },
+    modules: [
+        '@nuxt/content',
+        '@nuxt/ui',
+        '@vueuse/nuxt',
+        '@nuxt/image',
+        '@nuxthq/studio',
+        '@nuxtjs/web-vitals',
+        '@nuxtjs/fontaine',
+    ],
 
-  ui: {
-    icons: ['mdi']
-  },
+    pages: true,
 
-  image: {
-    quality: 80,
-    format: ['webp'],
-    dir: 'assets/images'
-  },
+    colorMode: {
+        preference: 'light'
+    },
 
-  webVitals: {
-    provider: 'log',
-    debug: true,
-    disabled: false
-  },
+    ui: {
+        icons: ['mdi']
+    },
 
-  compatibilityDate: '2024-08-05'
+    image: {
+        quality: 80,
+        format: ['webp'],
+        dir: 'assets/images'
+    },
+
+    webVitals: {
+        provider: 'log',
+        debug: true,
+        disabled: false
+    },
+
+    vite: {
+        plugins: [
+            viteCompression({
+                algorithm: 'gzip',
+                ext: '.gz',
+                threshold: 10240,
+            }),
+        ],
+    },
+
+    compatibilityDate: '2024-08-05'
 })
