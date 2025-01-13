866695e8b4a7bc5b3ae7f86cde4595a83a9b1a0b
c1chy-jott <paul@jott.media>
2025-01-13 17:06:46 +0100

    feat(app.vue): update styles and transition effects

--- app.vue
+++ app.vue
@@ -41,7 +41,7 @@ onUnmounted(() => {
 });
 </script>
 
-<style lang="scss">
+<style>
 
 @media (hover: hover) {
   * {
@@ -82,14 +82,14 @@ onUnmounted(() => {
     }
   }
 
-  .page-enter-active,
-  .page-leave-active {
-    transition: all 0.4s;
-  }
-  .page-enter-from,
-  .page-leave-to {
-    opacity: 0;
-    filter: blur(1rem);
-  }
+}
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
 }
 </style>
