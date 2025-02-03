a6e8dcc5db91802d655e02979703e774565d3e10
c1chy-jott <paul@jott.media>
2025-02-03 16:02:10 +0100

    feat(router.options): add comment to scrollBehavior function

--- app/router.options.ts
+++ app/router.options.ts
@@ -3,7 +3,7 @@ import type { RouterConfig } from "@nuxt/schema";
 export default <RouterConfig>{
    async scrollBehavior (to, from, savedPosition) {
         if (savedPosition) {
-            return savedPosition;
+            return savedPosition; // scroll to saved position
         }
     }
 };
