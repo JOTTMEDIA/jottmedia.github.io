5bb1a09412b42b3a9e1d2bd1ad9404ac494cb410
c1chy-jott <paul@jott.media>
2025-02-07 15:03:28 +0100

    feat(blog): refactor code structure in slug page

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,19 +1,19 @@
 <template>
   <UPage>
-    <UPageBody prose
-               class="prose-h1:normal-case prose-h1:font-normal prose-headings:uppercase prose-lead:uppercase prose-lead:text-base">
+    <UPageBody class="prose-h1:normal-case prose-h1:font-normal prose-headings:uppercase prose-lead:uppercase prose-lead:text-base"
+               prose>
       <UContainer :ui="{'constrained': 'max-w-2xl'}">
         <Center>
-          <NuxtLink to="/" class="inline-block no-underline border-0">
-            <Image src="logo.svg" alt="JOTT.MEDIA GmbH" class="w-[325px] mt-2" :shine="false" :parallax="false"/>
+          <NuxtLink class="inline-block no-underline border-0" to="/">
+            <Image :parallax="false" :shine="false" alt="JOTT.MEDIA GmbH" class="w-[325px] mt-2" src="logo.svg"/>
           </NuxtLink>
         </Center>
       </UContainer>
-      <UContainer class="pt-10" :ui="{'constrained': 'max-w-2xl'}">
-        <h1 v-html="page?.title" class="text-4xl"/>
+      <UContainer :ui="{'constrained': 'max-w-2xl'}" class="pt-10">
+        <h1 class="text-4xl" v-html="page?.title"/>
         <div class="flex gap-2 mb-2">
-          <UBadge :ui="{ rounded: 'rounded-lg', font: 'font-extrabold', size: { xs: 'text-xs px-3 py-0.5' } }"
-                  v-for="category in page?.categories" :key="category" :label="category" size="xs"/>
+          <UBadge v-for="category in page?.categories"
+                  :key="category" :label="category" :ui="{ rounded: 'rounded-lg', font: 'font-extrabold', size: { xs: 'text-xs px-3 py-0.5' } }" size="xs"/>
         </div>
         <small>{{ page?.date }} von
           <NuxtLink :to="`../team/${page?.author.toLowerCase()}`"><b class="text-jm-primary-green">{{ page?.author }}
@@ -22,19 +22,19 @@
         <p class="lead">{{ page?.description }}</p>
       </UContainer>
       <UContainer :ui="{'constrained': 'max-w-4xl'}">
-        <Image :src="page?.image" :public-src="true" :alt="page?.imageAlt" :hint="page?.imageAlt"/>
+        <Image :alt="page?.imageAlt" :hint="page?.imageAlt" :public-src="true" :src="page?.image"/>
       </UContainer>
-      <UContainer class="pb-10" :ui="{'constrained': 'max-w-2xl'}">
+      <UContainer :ui="{'constrained': 'max-w-2xl'}" class="pb-10">
         <ContentRenderer v-if="page?.body" :value="page"/>
       </UContainer>
     </UPageBody>
   </UPage>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 
 const route = useRoute()
-const {data: page} = await useAsyncData(route.path, () => queryContent(route.path).findOne())
+const {data: page} = await useAsyncData(route.path, () => queryCollection('content').first())
 
 useSeoMeta({
   title: page.value?.seoTitle + ' - Blog - JOTT.MEDIA',
