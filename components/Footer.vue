e96a27169f01f522fb8e699a3e7fa114cd753cd4
c1chy-jott <paul@jott.media>
2025-02-14 17:02:42 +0100

    feat: add i18n support to Footer.vue component

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="relative bg-jm-secondary-grey-lighter mt-40">
-    <Background height="127px" src="footer-top.svg" position="top" parallax="to-left" />
+    <Background height="127px" parallax="to-left" position="top" src="footer-top.svg"/>
 
     <div class="relative z-10">
 
 
-      <UContainer class="relative py-10 z-10" :ui="{'constrained': 'max-w-2xl'}">
+      <UContainer :ui="{'constrained': 'max-w-2xl'}" class="relative py-10 z-10">
         <div class="flex justify-between items-end">
           <div>
             <address class="not-italic">
@@ -13,26 +13,34 @@
               Bahnhofstraße 33<br>
               31675 Bückeburg<br>
               <br>
-              <nuxt-link href="tel:+4957229979070">+49 5722 99790 70</nuxt-link><br>
+              <nuxt-link href="tel:+4957229979070">+49 5722 99790 70</nuxt-link>
+              <br>
               <nuxt-link href="mailto:hallo@jott.media">hallo@jott.media</nuxt-link>
             </address>
           </div>
           <div class="text-right">
-            <nuxt-link to="/imprint" class="block font-[800] text-jm-primary-green uppercase">.Impressum</nuxt-link>
-            <nuxt-link to="/privacy" class="block font-[800] text-jm-primary-green uppercase">.Datenschutz</nuxt-link>
+            <nuxt-link class="block font-[800] text-jm-primary-green uppercase" to="/imprint">{{
+                t('imprint')
+              }}
+            </nuxt-link>
+            <nuxt-link class="block font-[800] text-jm-primary-green uppercase" to="/privacy">{{
+                t('privacy')
+              }}
+            </nuxt-link>
           </div>
         </div>
       </UContainer>
 
-      <Background width="400px" height="400px" src="footer-box.png" position="bottomRight" :out="false" />
+      <Background :out="false" height="400px" position="bottomRight" src="footer-box.png" width="400px"/>
     </div>
 
     <div class="bg-jm-primary-green text-sm py-5">
       <UContainer :ui="{'constrained': 'max-w-2xl'}">
-        <p>© {{ new Date().getFullYear() }} JOTT.MEDIA – Alle Rechte vorbehalten</p>
+        <p>© {{ new Date().getFullYear() }} JOTT.MEDIA – {{ t('rights') }}</p>
       </UContainer>
     </div>
   </div>
 </template>
-<script setup lang="ts">
+<script lang="ts" setup>
+const {t} = useI18n()
 </script>
