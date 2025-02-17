2ddf28010a350fcb05e3edfcc18315f90426c422
Jonathan Schneider <jonathan@jott.media>
2025-02-17 17:29:21 +0100

    fix: update @nuxt/content to version 3.1.1 and clean up nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,70 +2,79 @@
 
 export default defineNuxtConfig({
   devtools: {
-      enabled: true,
-      timeline: {
-          enabled: true
-      }
+    enabled: true,
+    timeline: {
+      enabled: true
+    }
   },
   routeRules: {
-      '/': {prerender: true},
+    '/': { prerender: true },
   },
 
   app: {
-      pageTransition: {name: 'page', mode: 'out-in'},
-      layoutTransition: { name: 'page', mode: 'out-in' },
-      head: {
-          title: 'Dein Büro für Entwicklung und Design – JOTT.MEDIA',
-          meta: [
-              {charset: 'utf-8'},
-              {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-              {
-                  name: 'description',
-                  content: 'Wir unterstützen dich und dein Unternehmen eigene Prozesse digital zu vereinfachen, um mehr Zeit, mehr Zufriedenheit und mehr Qualität zu gewinnen und erfoglreich zu wachsen.'
-              },
-              {name: 'format-detection', content: 'telephone=no'},
-              {name: 'msapplication-TileColor', content: '#ffffff'},
-              {name: 'theme-color', content: '#ffffff'},
-          ],
-          link: [
-              {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-              {
-                  rel: 'apple-touch-icon',
-                  sizes: '180x180',
-                  href: '/apple-touch-icon.png',
-              },
-              {
-                  rel: 'icon',
-                  type: 'image/png',
-                  sizes: '32x32',
-                  href: '/favicon-32x32.png',
-              },
-              {
-                  rel: 'icon',
-                  type: 'image/png',
-                  sizes: '16x16',
-                  href: '/favicon-16x16.png',
-              },
-              {rel: 'manifest', href: '/site.webmanifest'},
-              {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
-          ]
-      },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      title: 'Dein Büro für Entwicklung und Design – JOTT.MEDIA',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          name: 'description',
+          content: 'Wir unterstützen dich und dein Unternehmen eigene Prozesse digital zu vereinfachen, um mehr Zeit, mehr Zufriedenheit und mehr Qualität zu gewinnen und erfoglreich zu wachsen.'
+        },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'msapplication-TileColor', content: '#ffffff' },
+        { name: 'theme-color', content: '#ffffff' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      ]
+    },
   },
 
   extends: ['@nuxt/ui-pro'],
 
   modules: [
-      '@nuxt/content',
-      '@nuxt/ui',
-      '@vueuse/nuxt',
-      '@nuxt/image',
-      '@nuxthq/studio'
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@vueuse/nuxt',
+    '@nuxt/image',
   ],
   pages: true,
   colorMode: {
-      preference: 'light'
+    preference: 'light'
   },
-    uiPro: { routerOptions: false },
+  uiPro: { routerOptions: false },
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio',
+      gitInfo: {
+        name: 'jottmedia.github.io',
+        owner: 'JOTTMEDIA',
+        url: 'https://github.com/JOTTMEDIA/jottmedia.github.io'
+      }
+    }
+  },
 
   compatibilityDate: '2025-01-13'
 })
