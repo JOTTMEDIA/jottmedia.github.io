906ff8dca3450b50202857401e3aaf031b1f2d8c
c1chy-jott <paul@jott.media>
2025-02-20 19:03:53 +0100

    feat(ImageFigure.vue): add prefetch on interaction to NuxtLink

--- components/ImageFigure.vue
+++ components/ImageFigure.vue
@@ -2,7 +2,7 @@
   <div :class="typeToClass[align]" class="flex">
     <div class="md:w-2/5 space-y-10 relative">
       <NuxtImg :alt="hint ?? ''" :src="src" format="webp" loading="lazy" sizes="xs:100vw sm:50vw md:450px"/>
-      <NuxtLink :to="link">
+      <NuxtLink :to="link" prefetchOn="interaction">
         <button
             class="w-12 h-12 bg-jm-contrast-black bg-opacity-70 hover:bg-opacity-100 flex justify-center items-center rounded-full absolute sm:-right-6 top-0 sm:top-auto sm:-bottom-5  transition-all z-10">
           <UIcon class="text-2xl h-9 w-9 text-jm-primary-brown" name="i-mdi-plus"/>
