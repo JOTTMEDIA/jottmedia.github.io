c65c5c5f15d78cdc347eedc522a3e7f315d5ca87
c1chy-jott <paul@jott.media>
2025-02-20 16:55:44 +0100

    feat(Carousel.vue): improve image loading and swipe interaction
    This commit improves the image loading and swipe interaction in the Carousel.vue component. The image component has been replaced with NuxtImg for optimized image loading and the usePointerSwipe function has been improved to handle swipe direction more effectively. The layout has also been updated for better readability.

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="relative">
-    <button @click="x -= carousel?.children[0].clientWidth + 20" class="w-12 h-12 bg-jm-contrast-black bg-opacity-70 hover:bg-opacity-100 flex justify-center items-center rounded-full absolute left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all z-10"><UIcon name="i-mdi-chevron-left" class="text-3xl text-jm-primary-brown" /></button>
+    <button
+        class="w-12 h-12 bg-jm-contrast-black bg-opacity-70 hover:bg-opacity-100 flex justify-center items-center rounded-full absolute left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all z-10"
+        @click="x -= carousel?.children[0].clientWidth + 20">
+      <UIcon class="text-3xl text-jm-primary-brown" name="i-mdi-chevron-left"/>
+    </button>
     <div ref="carousel" class="flex flex-row overflow-x-auto no-scrollbar w-full gap-x-[20px] snap-mandatory snap-x">
-      <div v-for="item in items" :key="item.id" class="snap-start snap-always min-w-[100%] md:min-w-[calc(50%-10px)] pointer-events-none">
-        <Image :src="item.src" :alt="item.alt" :shine="false" :parallax="false" />
+      <div v-for="item in items" :key="item.id"
+           class="snap-start snap-always min-w-[100%] md:min-w-[calc(50%-10px)] pointer-events-none">
+        <NuxtImg :alt="item.alt" :src="item.src" class="w-full" format="webp" loading="lazy"/>
       </div>
     </div>
-    <button @click="x += carousel?.children[0].clientWidth + 20" class="w-12 h-12 bg-jm-contrast-black bg-opacity-70 hover:bg-opacity-100 flex justify-center items-center rounded-full absolute right-0 top-1/2 translate-x-1/2 -translate-y-1/2 transition-all z-10"><UIcon name="i-mdi-chevron-right" class="text-3xl text-jm-primary-brown" /></button>
+    <button
+        class="w-12 h-12 bg-jm-contrast-black bg-opacity-70 hover:bg-opacity-100 flex justify-center items-center rounded-full absolute right-0 top-1/2 translate-x-1/2 -translate-y-1/2 transition-all z-10"
+        @click="x += carousel?.children[0].clientWidth + 20">
+      <UIcon class="text-3xl text-jm-primary-brown" name="i-mdi-chevron-right"/>
+    </button>
   </div>
 </template>
 
-<script setup lang="ts">
-import { useScroll, usePointerSwipe } from '@vueuse/core'
+<script lang="ts" setup>
+import {usePointerSwipe, useScroll} from '@vueuse/core'
 
 interface CarouselItems {
   id: string
@@ -27,13 +36,13 @@ const props = defineProps({
 })
 
 const carousel = ref<HTMLElement | null>(null)
-const { x, y } = useScroll(carousel, { behavior: 'smooth' })
+const {x, y} = useScroll(carousel, {behavior: 'smooth'})
 usePointerSwipe(carousel, {
   disableTextSelect: true,
   onSwipeEnd(e: PointerEvent, direction) {
-    if(direction === 'left') {
+    if (direction === 'left') {
       x.value += (carousel.value?.children[0].clientWidth ?? 0) + 20
-    } else if(direction === 'right') {
+    } else if (direction === 'right') {
       x.value -= (carousel.value?.children[0].clientWidth ?? 0) + 20
     }
   },
