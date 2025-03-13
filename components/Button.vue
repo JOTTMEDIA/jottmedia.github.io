397b04f7af3de4f70c194dcf2d4d7effa9318042
c1chy-jott <paul@jott.media>
2025-03-13 19:18:15 +0100

    feat(Button.vue): update button component to use UButton with primary color and xl size

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,9 @@
 <template>
-  <button class="inline-block border-[3px] border-jm-primary-brown rounded px-3 py-2 uppercase font-[800] text-sm text-jm-primary-brown hover:text-jm-secondary-white hover:bg-jm-primary-brown transition">
-    <slot />
-  </button>
+  <UButton
+      color="primary"
+      outline="true"
+      size="xl"
+  >
+    <slot/>
+  </UButton>
 </template>
