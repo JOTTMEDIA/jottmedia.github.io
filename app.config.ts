7e39ff4289d38f5fbcb661089d756acbfc91ee4d
c1chy-jott <paul@jott.media>
2025-03-13 19:18:26 +0100

    feat(app.config.ts): update UI configuration
    Updated the UI configuration in app.config.ts to include container settings and color definitions. This change also includes adjustments to the light and dark mode color variables.

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,23 @@
 export default defineAppConfig({
-  ui: {
-    primary: 'nuxt-ui-primary',
-    // gray: 'nuxt-ui-grey',
-    variables: {
-      light: {
-        background: '255 255 255',
-        foreground: '000 000 000'
-      },
-      dark: {
-        background: '255 255 255',
-        foreground: '000 000 000'
-      },
+    ui: {
+        container: {
+            base: 'max-w-[var(--ui-container-base)] mx-auto px-4 sm:px-6 lg:px-8',
+            small: 'max-w-[var(--ui-container-small)] mx-auto px-4 sm:px-6 lg:px-8',
+        },
+        colors: {
+            primary: 'text-[var(--ui-text-primary)] bg-[var(--ui-bg-primary)]',
+            grey: 'nuxt-ui-grey'
+        },
+
+        variables: {
+            light: {
+                background: '255 255 255',
+                foreground: '000 000 000'
+            },
+            dark: {
+                background: '255 255 255',
+                foreground: '000 000 000'
+            },
+        }
     }
-  }
 })
