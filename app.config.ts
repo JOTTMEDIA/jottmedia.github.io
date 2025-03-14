2b94d8b7978a130af9a877f63697ba8b331bdb9c
c1chy-jott <paul@jott.media>
2025-03-14 19:14:27 +0100

    feat(app.config.ts): add new UI configurations
    Added new UI configurations for buttons and size in the app.config.ts file. This includes default variants, slots, and size attributes for xs, sm, md, lg, and xl. Also updated the primary and secondary color settings.

--- app.config.ts
+++ app.config.ts
@@ -5,10 +5,52 @@ export default defineAppConfig({
             small: 'max-w-[var(--ui-container-small)] mx-auto px-4 sm:px-6 lg:px-8',
         },
         colors: {
-            primary: 'text-[var(--ui-text-primary)] bg-[var(--ui-bg-primary)]',
-            grey: 'nuxt-ui-grey'
+            primary: 'nuxt-ui-primary',
+            secondary: 'nuxt-ui-brown',
+
+        },
+        button: {
+            defaultVariants: {
+                color: "secondary",
+            },
+            slots: {
+                base: 'inline-block border-[3px] border-jm-primary-brown rounded px-3 py-2 uppercase font-[800] text-sm text-jm-primary-brown hover:text-jm-secondary-white hover:bg-jm-primary-brown transition',
+            }
+
         },
 
+        size: {
+            xs: {
+                base: 'px-2 py-1 text-xs gap-1',
+                leadingIcon: 'size-4',
+                leadingAvatarSize: '3xs',
+                trailingIcon: 'size-4'
+            },
+            sm: {
+                base: 'px-2.5 py-1.5 text-xs gap-1.5',
+                leadingIcon: 'size-4',
+                leadingAvatarSize: '3xs',
+                trailingIcon: 'size-4'
+            },
+            md: {
+                base: 'px-2.5 py-1.5 text-sm gap-1.5',
+                leadingIcon: 'size-5',
+                leadingAvatarSize: '2xs',
+                trailingIcon: 'size-5'
+            },
+            lg: {
+                base: 'px-3 py-2 text-sm gap-2',
+                leadingIcon: 'size-5',
+                leadingAvatarSize: '2xs',
+                trailingIcon: 'size-5'
+            },
+            xl: {
+                base: 'px-3 py-2 text-base gap-2',
+                leadingIcon: 'size-6',
+                leadingAvatarSize: 'xs',
+                trailingIcon: 'size-6'
+            }
+        },
         variables: {
             light: {
                 background: '255 255 255',
