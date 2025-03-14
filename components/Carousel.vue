5c52de2a93835fa2a58d5433c36d9a8b8e1a1010
c1chy-jott <paul@jott.media>
2025-03-14 19:14:36 +0100

    feat: improve Carousel.vue UX and accessibility
    The Carousel.vue component has been updated to improve user experience and accessibility. Button elements have been refactored to use UButton component for better consistency and maintainability. The on-click events have been adjusted to provide a smoother transition. The usePointerSwipe event has been updated to provide better responsiveness. Code readability has also been improved with better formatting and organization.

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="relative">
-    <button @click="x -= carousel?.children[0].clientWidth + 20" class="w-12 h-12 bg-jm-contrast-black bg-opacity-70 hover:bg-opacity-100 flex justify-center items-center rounded-full absolute left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all z-10"><UIcon name="i-mdi-chevron-left" class="text-3xl text-jm-primary-brown" /></button>
+    <UButton
+        :style="{ backgroundColor: 'var(--color-jm-contrast-black)'}"
+        :ui="{ base: 'h-12 w-12 opacity-70 flex justify-center border-0 items-center transition-opacity duration-300 ease rounded-full absolute left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full transition-all z-10' }"
+        class="hover:!opacity-100"
+        outline="false"
+        size="lg"
+        variant="outline"
+        @click="x -= carousel?.children[0].clientWidth + 20"
+    >
+      <UIcon
+          class="bg-[var(--color-nuxt-ui-brown-50)] size-7"
+          name="i-mdi-chevron-left"
+      />
+    </UButton>
     <div ref="carousel" class="flex flex-row overflow-x-auto no-scrollbar w-full gap-x-[20px] snap-mandatory snap-x">
-      <div v-for="item in items" :key="item.id" class="snap-start snap-always min-w-[100%] md:min-w-[calc(50%-10px)] pointer-events-none">
-        <Image :src="item.src" :alt="item.alt" :shine="false" :parallax="false" />
+      <div v-for="item in items" :key="item.id"
+           class="snap-start snap-always min-w-[100%] md:min-w-[calc(50%-10px)] pointer-events-none">
+        <Image :alt="item.alt" :parallax="false" :shine="false" :src="item.src"/>
       </div>
     </div>
-    <button @click="x += carousel?.children[0].clientWidth + 20" class="w-12 h-12 bg-jm-contrast-black bg-opacity-70 hover:bg-opacity-100 flex justify-center items-center rounded-full absolute right-0 top-1/2 translate-x-1/2 -translate-y-1/2 transition-all z-10"><UIcon name="i-mdi-chevron-right" class="text-3xl text-jm-primary-brown" /></button>
+    <UButton
+        :style="{ backgroundColor: 'var(--color-jm-contrast-black)'}"
+        :ui="{ base: 'h-12 w-12 opacity-70 flex justify-center border-0 items-center transition-opacity duration-300 ease rounded-full absolute right-0 top-1/2 translate-x-1/2 -translate-y-1/2 rounded-full transition-all z-10' }"
+        class="hover:!opacity-100"
+
+        outline="false"
+        size="lg"
+        variant="outline"
+        @click="x += carousel?.children[0].clientWidth + 20"
+    >
+      <UIcon
+          class="bg-[var(--color-nuxt-ui-brown-50)]"
+          name="i-mdi-chevron-right"
+          size="2.1rem"/>
+    </UButton>
   </div>
 </template>
 
-<script setup lang="ts">
-import { useScroll, usePointerSwipe } from '@vueuse/core'
+<script lang="ts" setup>
+import {usePointerSwipe, useScroll} from '@vueuse/core'
 
 interface CarouselItems {
   id: string
@@ -27,13 +55,13 @@ const props = defineProps({
 })
 
 const carousel = ref<HTMLElement | null>(null)
-const { x, y } = useScroll(carousel, { behavior: 'smooth' })
+const {x, y} = useScroll(carousel, {behavior: 'smooth'})
 usePointerSwipe(carousel, {
   disableTextSelect: true,
   onSwipeEnd(e: PointerEvent, direction) {
-    if(direction === 'left') {
+    if (direction === 'left') {
       x.value += (carousel.value?.children[0].clientWidth ?? 0) + 20
-    } else if(direction === 'right') {
+    } else if (direction === 'right') {
       x.value -= (carousel.value?.children[0].clientWidth ?? 0) + 20
     }
   },
