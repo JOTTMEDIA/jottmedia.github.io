9a469f11274202a40c29a7399ae680aa846551cd
c1chy-jott <paul@jott.media>
2025-03-26 19:07:48 +0100

    feat: refactor Menu.vue layout structure

--- components/Menu.vue
+++ components/Menu.vue
@@ -15,66 +15,66 @@
       <UIcon v-else class="text-lg sm:text-2xl text-jm-primary-brown transition-all z-50" name="i-mdi-close-thick"/>
     </UButton>
     <div class="relative py-3 mx-auto ">
-      <Center>
-        <transition mode="in-out" name="page">
-          <div
-              v-if="open"
-              class="top-0 left-0 w-screen h-screen flex flex-col justify-start  z-30 bg-jm-primary-brown fixed">
-            <UContainer :ui="{'constrained': 'sm:max-w-6xl'}" class="my-8">
-              <Center>
-                <NuxtLink class="inline-block no-underline w-full mt-2 sm:w-[325px] border-0" to="/"
-                          @click="open = false">
-                  <Image :parallax="false" :shine="false" alt="JOTT.MEDIA GmbH" class="w-full"
-                         src="logo-overlay.svg"/>
-                </NuxtLink>
-              </Center>
-            </UContainer>
 
-            <UContainer :ui="{'constrained': 'w-full', padding: 'space-x-2 sm:space-x-4'}"
-                        class="sm:mt-12 flex flex-col sm:flex-row justify-around">
-              <ul class="uppercase font-extrabold text-left text-xl xl:text-3xl space-y-4 sm:space-y-8 text-jm-contrast-black">
-                <li>
-                  <NuxtLink to="/" @click="open = false">.Startseite</NuxtLink>
-                </li>
-                <li>
-                  <NuxtLink to="/blog" @click="open = false">.Blog</NuxtLink>
-                </li>
-              </ul>
-              <div class="text-left sm:text-right mt-8 sm:mt-0">
-                <nuxt-link class="block font-extrabold text-sm sm:text-base" to="/privacy" @click="open = false">
-                  Datenschutz
-                </nuxt-link>
-                <nuxt-link class="block font-extrabold text-sm sm:text-base" to="/imprint" @click="open = false">
-                  Impressum
-                </nuxt-link>
-                <address class="not-italic text-sm  mt-6 sm:mt-16">
-                  JOTT.MEDIA GmbH<br>
-                  Bahnhofstraße 33<br>
-                  31675 Bückeburg<br>
-                  <br>
-                  <nuxt-link href="tel:+4957229184984">+49 5722 9184984</nuxt-link>
-                  <br>
-                  <nuxt-link href="mailto:hallo@jott.media">hallo@jott.media</nuxt-link>
-                  <br>
-                  <br>
-                  <div class="text-left sm:text-right flex sm:justify-end items-center space-x-1">
-                    <p class="pr-2">Folge uns:</p>
-                    <NuxtLink class="inline-block no-underline border-0" href="https://www.instagram.com/jott.media/"
-                              target="_blank">
-                      <UIcon class="text-[#1E1E1E] text-2xl block " name="i-mdi-instagram"/>
-                    </NuxtLink>
-                    <NuxtLink class="inline-block no-underline border-0"
-                              href="https://de.linkedin.com/company/jottmedia"
-                              target="_blank">
-                      <UIcon class="text-[#1E1E1E] text-2xl block" name="i-mdi-linkedin"/>
-                    </NuxtLink>
-                  </div>
-                </address>
-              </div>
-            </UContainer>
-          </div>
-        </transition>
-      </Center>
+      <transition mode="in-out" name="page">
+        <div
+            v-if="open"
+            class="top-0 left-0 w-screen h-screen flex flex-col justify-start  z-30 bg-[var(--color-jm-primary-brown)] fixed">
+          <UContainer :ui="{'constrained': 'sm:max-w-6xl'}" class="my-8">
+
+            <NuxtLink class="inline-block no-underline w-full mt-2 sm:w-[325px] border-0" to="/"
+                      @click="open = false">
+              <Image :parallax="false" :shine="false" alt="JOTT.MEDIA GmbH" class="w-full"
+                     src="logo-overlay.svg"/>
+            </NuxtLink>
+
+          </UContainer>
+
+          <UContainer :ui="{'constrained': 'w-full', padding: 'space-x-2 sm:space-x-4'}"
+                      class="sm:mt-12 flex flex-col sm:flex-row justify-around">
+            <ul class="uppercase font-extrabold text-left text-xl xl:text-3xl space-y-4 sm:space-y-8 text-jm-contrast-black">
+              <li>
+                <NuxtLink to="/" @click="open = false">.Startseite</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/blog" @click="open = false">.Blog</NuxtLink>
+              </li>
+            </ul>
+            <div class="text-left sm:text-right mt-8 sm:mt-0">
+              <nuxt-link class="block font-extrabold text-sm sm:text-base" to="/privacy" @click="open = false">
+                Datenschutz
+              </nuxt-link>
+              <nuxt-link class="block font-extrabold text-sm sm:text-base" to="/imprint" @click="open = false">
+                Impressum
+              </nuxt-link>
+              <address class="not-italic text-sm  mt-6 sm:mt-16">
+                JOTT.MEDIA GmbH<br>
+                Bahnhofstraße 33<br>
+                31675 Bückeburg<br>
+                <br>
+                <nuxt-link href="tel:+4957229184984">+49 5722 9184984</nuxt-link>
+                <br>
+                <nuxt-link href="mailto:hallo@jott.media">hallo@jott.media</nuxt-link>
+                <br>
+                <br>
+                <div class="text-left sm:text-right flex sm:justify-end items-center space-x-1">
+                  <p class="pr-2">Folge uns:</p>
+                  <NuxtLink class="inline-block no-underline border-0" href="https://www.instagram.com/jott.media/"
+                            target="_blank">
+                    <UIcon class="text-[#1E1E1E] text-2xl block " name="i-mdi-instagram"/>
+                  </NuxtLink>
+                  <NuxtLink class="inline-block no-underline border-0"
+                            href="https://de.linkedin.com/company/jottmedia"
+                            target="_blank">
+                    <UIcon class="text-[#1E1E1E] text-2xl block" name="i-mdi-linkedin"/>
+                  </NuxtLink>
+                </div>
+              </address>
+            </div>
+          </UContainer>
+        </div>
+      </transition>
+
     </div>
   </div>
 </template>
