5f88453620ba9f60db626723732690c065865271
c1chy-jott <paul@jott.media>
2025-04-02 18:54:07 +0200

    feat: update UI styles and layouts in blog pages

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,37 +1,37 @@
 <template>
-  <UPage>
+  <UPage class="m-0 p-0 bg-(--color-jm-secondary-white)">
     <UPageBody
         class="prose-h1:normal-case prose-h1:font-normal prose-headings:uppercase prose-lead:uppercase prose-lead:text-base"
         prose>
-      <UContainer :ui="{'constrained': 'max-w-2xl'}">
+      <UContainer class="max-w-(--container-2xl)">
 
         <NuxtLink class="inline-block no-underline border-0" to="/">
           <Image :parallax="false" :shine="false" alt="JOTT.MEDIA GmbH" class="w-[325px] mt-2" src="logo.svg"/>
         </NuxtLink>
 
       </UContainer>
-      <UContainer :ui="{'constrained': 'max-w-2xl'}" class="pt-10">
+      <UContainer class="pt-10 max-w-(--container-2xl)">
         <h1 class="text-4xl" v-html="page?.title"/>
         <div class="flex gap-2 mb-2">
           <UBadge v-for="category in page?.meta.categories"
                   :key="category" :label="category"
-                  :ui="{ rounded: 'rounded-lg', font: 'font-extrabold', size: { xs: 'text-xs px-3 py-0.5' } }"
+                  :ui="{ rounded: 'rounded-lg', font: 'font-extrabold', size: { xs: 'text-sm px-3 py-0.5' } }"
                   size="xs"/>
         </div>
         <small>{{ page?.meta.date }} von
           <NuxtLink :to="`../team/${(page?.meta.author as string).toLowerCase()}`"><b
-              class="text-jm-primary-green">{{ page?.meta.author }}
+              class="text-(--color-jm-primary-green)">{{ page?.meta.author }}
           </b></NuxtLink>
         </small>
         <p class="lead">{{ page?.description }}</p>
       </UContainer>
-      <UContainer :ui="{'constrained': 'max-w-4xl'}">
+      <UContainer class="max-w-(--container-4xl)">
         <Image :alt="page?.meta.imageAlt as string | undefined"
                :hint="page?.meta.imageAlt as string | undefined"
                :public-src="true"
                :src="page?.meta.image as string"/>
       </UContainer>
-      <UContainer :ui="{'constrained': 'max-w-2xl'}" class="pb-10">
+      <UContainer class="pb-10 max-w-(--container-2xl)">
         <ContentRenderer v-if="page?.body" :value="page"/>
       </UContainer>
     </UPageBody>
