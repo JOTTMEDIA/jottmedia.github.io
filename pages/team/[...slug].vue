c3fbf0c3e23bf012ee56f01de2d80795fd3fee1b
c1chy-jott <paul@jott.media>
2025-04-04 19:01:40 +0200

    feat: update team page layout for better responsiveness
    This commit updates the team page layout to improve its responsiveness across different screen sizes. The changes include modifying CSS classes for better alignment and visibility on small screens, and adjusting text size for readability.

--- pages/team/[...slug].vue
+++ pages/team/[...slug].vue
@@ -1,15 +1,15 @@
 <template>
-  <UPageBody class="m-0 p-0 h-screen flex space-y-0">
-    <div class="w-1/2">
+  <UPageBody class="m-0 p-0 h-screen flex flex-col sm:flex-row space-y-0">
+    <div class="sm:w-1/2">
       <Image :cover="true" :parallax="false" :shine="false" :src="person?.meta?.src as string"
-             class="w-full h-full"/>
+             class="w-full h-full "/>
     </div>
     <div
         :class="person?.meta.bg"
-        class="w-1/2 flex flex-col justify-center px-4 space-y-4 lg:px-6 lg:space-y-6 xl:space-y-8 xl:px-8 2xl:space-y-12 2xl:px-20">
+        class="sm:w-1/2 flex flex-col justify-center p-4 space-y-4 lg:px-6 lg:space-y-6 xl:space-y-8 xl:px-8 2xl:space-y-12 2xl:px-20">
       <UButton
           :style="{ backgroundColor: 'var(--color-jm-contrast-black)', position : 'fixed' }"
-          :ui="{ base: 'fixed h-10 w-10 border-0 -bottom-6 right-4 sm:right-10 sm:h-12 sm:w-12 flex justify-center items-center transition-all duration-700 opacity-70 rounded-full z-50' }"
+          :ui="{ base: 'fixed h-10 w-10 border-0 bottom-0 right-2 xl:bottom-1  lg:right-4 xl:right-10 sm:h-12 sm:w-12 flex justify-center items-center transition-all duration-700 opacity-70 rounded-full z-50' }"
           class="hover:!opacity-100"
           color="secondary"
           label="Einfach machen"
@@ -22,8 +22,8 @@
       </UButton>
 
       <h1 class="text-lg xl:text-2xl uppercase font-extrabold" v-html="person?.meta.name"/>
-      <p class="font-light" v-html="person?.description"></p>
-      <UContainer class="relative pt-4 z-10">
+      <p class="font-light text-sm xl:text-base" v-html="person?.description"></p>
+      <UContainer class="relative pt-4 text-center z-10">
         <UButton
             :class="person?.meta.button"
             color="secondary"
