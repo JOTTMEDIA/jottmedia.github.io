adc22ec286eadb84b5f46fa3c957392f39f9d808
c1chy-jott <paul@jott.media>
2025-05-09 18:04:27 +0200

    Add LinkedIn OAuth callback page
    Create a new callback page to handle LinkedIn OAuth responses. The page extracts the authorization code from the query parameters, sends it to the backend to fetch an access token, and then redirects to the homepage.

---
+++ pages/linkedin/callback.vue
@@ -0,0 +1,28 @@
+<!-- pages/linkedin/callback.vue -->
+<script setup>
+
+
+const route = useRoute()
+const router = useRouter()
+
+onMounted(async () => {
+  const code = route.query.code
+  const state = route.query.state
+
+  if (!code) return
+
+  const res = await fetch('/api/linkedin/token', {
+    method: 'POST',
+    body: {code}
+  })
+  console.log(code)
+  console.log('Access token:', res.access_token)
+
+
+  router.push('/')
+})
+</script>
+
+<template>
+  <p>Logowanie z LinkedIn...</p>
+</template>
