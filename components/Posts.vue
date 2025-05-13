192b958cc88095d9052dc19afef3f4b15129e598
c1chy-jott <paul@jott.media>
2025-05-13 18:12:25 +0200

    feat: add Posts.vue component for displaying posts

---
+++ components/Posts.vue
@@ -0,0 +1,17 @@
+<script setup>
+const {data: posts, pending, error} = await useFetch('/api/posts');
+</script>
+
+<template>
+  <div>
+    <h1>Google Business Neuigkeiten</h1>
+    <div v-if="pending">Wird geladen...</div>
+    <div v-else-if="error">Fehler: {{ error.message }}</div>
+    <ul v-else>
+      <li v-for="post in posts.localPosts" :key="post.name">
+        <h3>{{ post.summary }}</h3>
+        <p>Aktion: {{ post.callToAction?.actionType }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
