21c8abfc26044f7003a2326ca7d89e4c3a708391
c1chy-jott <paul@jott.media>
2025-07-09 17:42:39 +0200

    feat: add app configuration for UI components and styling
    Introduce `app.config.ts` to define configurations for `uiPro` and `ui` components, including slots, variants, colors, sizes, and variables to enhance customizability and maintainability.

---
+++ app/app.config.ts
@@ -0,0 +1,112 @@
+export default defineAppConfig({
+    uiPro: {
+        blogPost: {
+            slots: {
+                authors: 'pt-4',
+                body: 'sm:p-3!',
+                description: 'mb-0 text-xs text-pretty jm-contrast-black color-jm-contrast-black',
+                image: 'object-fill w-full max-w-full h-full block bg-center bg-cover bg-no-repeat',
+                meta: 'gap-1 px-0',
+                root: 'relative group/blog-post flex flex-col overflow-hidden p-0 test',
+                title: 'text-xl uppercase font-extrabold leading-6',
+            },
+        },
+        pageHero: {
+            slots: {
+                root: 'relative isolate',
+                wrapper: '',
+                headline: 'mb-4',
+            },
+            variants: {
+                orientation: {
+                    horizontal: {
+                        container: 'lg:grid-cols-2 lg:items-center',
+                        description: 'text-pretty'
+                    },
+                    vertical: {
+                        container: '',
+                        headline: 'justify-center',
+                        wrapper: 'text-center',
+                        description: 'text-balance',
+                        links: 'justify-center'
+                    }
+                },
+                reverse: {
+                    true: {
+                        wrapper: 'order-last'
+                    }
+                },
+                headline: {
+                    true: {
+                        headline: 'font-semibold text-primary flex items-center gap-1.5'
+                    }
+                },
+                title: {
+                    true: {
+                        description: 'mt-6'
+                    }
+                }
+            }
+        }
+    },
+    ui: {
+        container: {
+            base: 'max-w-(--container-3xl) mx-auto px-4 sm:px-6 lg:px-8',
+        },
+        colors: {
+            primary: 'nuxt-ui-primary',
+            secondary: 'nuxt-ui-brown',
+        },
+        button: {
+            defaultVariants: {
+                color: "secondary",
+            },
+            slots: {
+                base: 'inline-block border-[3px] border-jm-primary-brown rounded px-3 py-2 uppercase font-[800] text-sm text-jm-primary-brown hover:text-jm-secondary-white hover:bg-jm-primary-brown transition',
+            }
+        },
+        size: {
+            xs: {
+                base: 'px-2 py-1 text-xs gap-1',
+                leadingIcon: 'size-4',
+                leadingAvatarSize: '3xs',
+                trailingIcon: 'size-4'
+            },
+            sm: {
+                base: 'px-2.5 py-1.5 text-xs gap-1.5',
+                leadingIcon: 'size-4',
+                leadingAvatarSize: '3xs',
+                trailingIcon: 'size-4'
+            },
+            md: {
+                base: 'px-2.5 py-1.5 text-sm gap-1.5',
+                leadingIcon: 'size-5',
+                leadingAvatarSize: '2xs',
+                trailingIcon: 'size-5'
+            },
+            lg: {
+                base: 'px-3 py-2 text-sm gap-2',
+                leadingIcon: 'size-5',
+                leadingAvatarSize: '2xs',
+                trailingIcon: 'size-5'
+            },
+            xl: {
+                base: 'px-3 py-2 text-base gap-2',
+                leadingIcon: 'size-6',
+                leadingAvatarSize: 'xs',
+                trailingIcon: 'size-6'
+            }
+        },
+        variables: {
+            light: {
+                background: '255 255 255',
+                foreground: '000 000 000'
+            },
+            dark: {
+                background: '255 255 255',
+                foreground: '000 000 000'
+            },
+        }
+    },
+
+})
