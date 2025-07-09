96dfe071f8ea3bf43a8eaebb6c7f9b0ab675f6cf
c1chy-jott <paul@jott.media>
2025-07-09 17:39:36 +0200

    feat: clean up and remove deprecated files and unused components
    Delete unused routes, components, and styles, including API routes, blog-related pages, and shared assets. Simplify codebase by removing outdated functionality and unused configurations.

--- components/Button.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <button class="inline-block border-[3px] border-jm-primary-brown rounded px-3 py-2 uppercase font-[800] text-sm text-jm-primary-brown hover:text-jm-secondary-white hover:bg-jm-primary-brown transition">
-    <slot />
-  </button>
-</template>
