ae817c3daeb62bed0b895c827ccb67d73fc9ba3d
c1chy-jott <paul@jott.media>
2025-07-09 17:43:55 +0200

    feat: add initial app structure with pages and components
    Introduce `app` directory to house new application structure, including pages for `index`, `blog`, and `team`. Add reusable components, refactor styles, and enhance SEO with meta tags and improved accessibility for content.

---
+++ app/pages/index.vue
@@ -0,0 +1,167 @@
+<script lang="ts" setup>
+
+useHead({
+  title: 'Dein Büro für Entwicklung und Design – JOTT.MEDIA'
+})
+
+function scrollTo(event: Event) {
+  event.preventDefault()
+  const target = document.getElementById('machen')
+  if (target) {
+    target.scrollIntoView({behavior: 'smooth'})
+  }
+}
+</script>
+<template>
+  <UPageBody class="h-screen text-center bg-(--color-jm-secondary-white)">
+    <div class="relative bg-(--color-jm-secondary-grey-lighter)">
+      <Background :out="false" height="1100px"  position="bottomLeft" src="header-box.png" width="1100px"/>
+      <Background :out="false" height="701px"   parallax="to-right" position="bottom" src="header-grey.svg"/>
+      <UPageHero
+          class="py-4 z-10 relative"
+          orientation="vertical"
+      >
+        <template #top>
+          <NuxtLink class="inline-block no-underline border-0" to="/">
+            <Image :parallax="false" :shine="false" alt="JOTT.MEDIA GmbH" src="logo.svg"/>
+          </NuxtLink>
+
+        </template>
+        <template #headline>
+          <h5 class="animated-bold text-5xl">
+            Machen wir es<br><span class="text-(--color-nuxt-ui-primary-50) uppercase">einfach:</span><b
+              class="uppercase"> digital</b>.
+          </h5>
+        </template>
+        <template #body>
+          <UButton
+              :to="{path: '/', hash: '#machen'}"
+              class="hover:bg-(--color-jm-primary-brown)/75 hover:text-(--ui-bg) hover:border-(--color-jm-primary-brown)"
+              color="secondary"
+              label="Einfach machen"
+              outline="false"
+              size="lg"
+              variant="outline"
+          />
+        </template>
+        <template #footer>
+          <div>
+            <h2 class="animated-bold uppercase text-2xl xl:text-3xl mb-0">Dein Büro<span
+                class="lowercase">für</span> <b>Entwicklung und Design.</b>
+            </h2>
+            <NuxtLink :to="{ path: '/', hash: '#machen' }" @click="scrollTo">
+              <UIcon class="text-(--color-jm-secondary-grey) text-[70px]" name="i-mdi-arrow-down"/>
+            </NuxtLink>
+          </div>
+        </template>
+      </UPageHero>
+    </div>
+    <div class="relative bg-(--color-jm-primary-green) text-left">
+      <Background height="644px" parallax="to-left" position="top" src="header-green-top.svg"/>
+      <UContainer id="machen" class="relative py-24 min-h-[400px]">
+        <h6>Das bekommst du <b class="uppercase">von uns</b></h6>
+        <h3><b>Mehr Zeit</b>, sodass du deine Energie auf den <b>Erfolg</b> und das Wachstum deines
+          Unternehmens fokussieren kannst. Dazu vereinfachen und digitalisieren wir deine Prozesse – und sorgen so für
+          <b>mehr Effizienz, Zufriedenheit und Qualität</b> für dein Business.
+        </h3>
+      </UContainer>
+      <Background height="875px" parallax="to-left" position="bottom" src="header-green-bottom.svg"/>
+    </div>
+
+    <UContainer class="max-w-(--container-4xl) relative py-10">
+      <Image alt="Arian und Jan im Termin" src="team.jpg"/>
+    </UContainer>
+
+    <UContainer class="xl:max-w-(--container-2xl) w-2/3  mx-0 lg:mx-auto text-left relative py-10">
+      <h6 class="animated-bold animation-h1">Die wichtigste <b class="uppercase">Frage</b></h6>
+      <h2 class="font-[400]">„<b class="uppercase">Was</b> genau <b class="uppercase">braucht dein
+        Unternehmen wirklich?</b>“
+      </h2>
+    </UContainer>
+
+    <UContainer class="max-w-(--container-2xl) relative pt-4 text-left">
+      <h6>So arbeiten wir <b class="uppercase">für Dich</b></h6>
+      <h4 class="text-(--color-jm-primary-brown)"><b>Ehrlich. zuverlässig.
+        partnerschaftlich.</b>
+      </h4>
+      <Paragraph>Die Lösung muss immer zum Problem passen. Darum steht bei uns <b>an erster Stelle</b> die Analyse
+        deiner Herausforderung. Erst, wenn diese glasklar ist, widmen wir uns der <b>Konzeption und Umsetzung</b> des
+        tatsächlichen Produkts, wie auch immer das in deinem konkreten Fall aussehen mag. Damit das funktioniert, ist
+        eine ehrliche, zuverlässige und partnerschaftliche Zusammenarbeit – <b>auf Augenhöhe</b> und auf beiden Seiten
+        – unabdingbar.
+      </Paragraph>
+    </UContainer>
+    <UButton
+        color="secondary"
+        label="Lass uns gemeinsam loslegen"
+        outline="true"
+        size="lg"
+        target="_blank"
+        to="https://calendar.app.google/rBDjAnPNYEQpfMvJ9"
+        variant="outline"
+    >
+    </UButton>
+    <UContainer class="max-w-(--container-2xl) relative xl:pt-4 z-10">
+      <UButton
+          color="secondary"
+          label="Lerne uns kennen"
+          outline="true"
+          size="lg"
+          target="_blank"
+          to="https://calendar.app.google/rBDjAnPNYEQpfMvJ9"
+          variant="outline"
+      ></UButton>
+    </UContainer>
+
+    <div class="relative bg-(--color-jm-primary-green) mt-64">
+      <UContainer class="max-w-(--container-xl) relative px-2 py-10 text-left">
+        <h6>Das leisten wir <b class="uppercase">für dich</b></h6>
+        <h2 class="font-[400] uppercase">Wir entwickeln und designen <b
+            class="text-(--color-jm-primary-brown)">digitale
+          Premiumlösungen</b> <b>individuell für dein Unternehmen.</b></h2>
+      </UContainer>
+    </div>
+
+    <UContainer class="max-w-(--container-2xl) pt-6 text-left">
+      <Paragraph><b class="text-(--color-jm-primary-brown) uppercase">Konzeption, Prototyping, Programmierung &
+        Design</b> von
+        <b>digitalen Lösungen</b>, mit denen du deine Herausforderungen optimal <b>meisterst</b>. Anders gesagt: <b>wir
+          machen Kompliziertes einfach</b> – und räumen Wachstumsbremsen aus dem Weg. <b>Ganz gleich, ob Webseite,
+          Web-Applikation oder App</b>: Unsere Lösungen zeichnen sich durch beeindruckende Funktionalität,
+        begeisternde Userfreundlichkeit und <b>bestechende Ästhetik</b> aus. Hand drauf.
+      </Paragraph>
+    </UContainer>
+    <UButton
+        color="secondary"
+        label="Los geht's"
+        outline="true"
+        size="lg"
+        target="_blank"
+        to="https://calendar.app.google/rBDjAnPNYEQpfMvJ9"
+        variant="outline"
+    >
+    </UButton>
+    <UContainer class="max-w-(--container-5xl) py-20">
+      <h2 class="pb-8 leading-9 xl:leading-5 text-3xl xl:text-4xl lowercase">
+        <b class="text-(--color-jm-primary-brown) uppercase">Neues</b> aus der
+        <b class="text-(--color-jm-primary-brown) uppercase"> digitalen Welt </b>
+      </h2>
+      <Content/>
+      <UButton
+          class="mt-8"
+          color="secondary"
+          label="Zum Blog"
+          outline="true"
+          size="lg"
+          to="blog"
+          variant="outline"
+      >
+      </UButton>
+    </UContainer>
+  </UPageBody>
+
+</template>
+
+<style scoped>
+
+</style>
--- app/pages/team/[...slug].vue
+++ app/pages/team/[...slug].vue
@@ -2,28 +2,28 @@
   <UPage>
     <UPageBody class="mt-0 pb-0 h-screen">
       <UContainer
-          :ui="{'constrained': 'max-w-full gap-0 px-0 sm:px-0 lg:px-0 '}"
-          class="flex flex-col sm:flex-row sm:h-screen">
+          class="gap-0 px-0 sm:px-0 lg:px-0 flex flex-col sm:flex-row sm:h-screen max-w-(--container-full) ">
         <div class="sm:w-1/2 h-1/2 sm:h-full">
-          <Image :cover="true" :parallax="false" :shine="false" :src="person?.meta?.src as string"
-                 class="w-full h-full"/>
+          <NuxtImg
+              :src="person?.meta.src"
+              format="webp"
+              :alt="person?.meta.imageAlt || person?.meta.name"
+              class="w-full h-full"/>
         </div>
         <div
-            :class="person?.meta.bg"
+            :class="`bg-(--color-${person?.meta.bg})`"
             class="sm:w-1/2 h-1/2 sm:h-full flex flex-col justify-center px-4 space-y-4 lg:px-6 lg:space-y-6 xl:space-y-8 xl:px-8 2xl:space-y-12 2xl:px-20">
           <button
               class="fixed bottom-3 sm:bottom-6 right-4 sm:right-10 h-10 w-10 sm:h-12 sm:w-12 flex justify-center items-center bg-jm-contrast-black transition-all duration-700 bg-opacity-70 hover:bg-opacity-100  rounded-full z-50"
-              @click="navigateBack">
+              @click="$router.back()">
             <UIcon class="text-lg sm:text-3xl text-jm-primary-brown transition-all z-50" name="i-mdi-close-thick"/>
           </button>
           <h1 class="text-lg xl:text-xl uppercase font-extrabold" v-html="person?.meta.name"/>
-          <p class="font-light" v-html="person?.description"></p>
+          <p class="font-light" v-html="person?.seo.description"></p>
           <UContainer class="relative pt-0 z-10">
-            <Center>
-              <NuxtLink href="https://calendar.app.google/rBDjAnPNYEQpfMvJ9" target="_blank">
-                <Button :class="person?.meta.button" class=" text-xs lg:text-base my-4">Lerne uns kennen</Button>
-              </NuxtLink>
-            </Center>
+            <NuxtLink href="https://calendar.app.google/rBDjAnPNYEQpfMvJ9" target="_blank">
+              <Button :class="person?.meta.button" class=" text-xs lg:text-base my-4">Lerne uns kennen</Button>
+            </NuxtLink>
           </UContainer>
         </div>
       </UContainer>
@@ -32,20 +32,20 @@
 </template>
 
 <script lang="ts" setup>
+import { useTeamStore } from '~/stores/teamStore'
+
 definePageMeta({
   layout: 'minimal'
 });
+
+const teamStore = useTeamStore()
 const route = useRoute()
-const router = useRouter()
 
-function navigateBack() {
-  router.back();
-}
+console.log('Current route path:', route.path)
 
-const {data: person} = await useAsyncData(() => {
-  return queryCollection('team')
-      .path(route.path)
-      .first()
-})
+await teamStore.fetchPerson(route.path)
 
+const person = computed(() => teamStore.person)
+
+console.log('Person from store:', person.value)
 </script>
