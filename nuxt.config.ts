e77f4e86ad563cb75c6a1528bed13db4508b6d0b
c1chy-jott <paul@jott.media>
2025-07-09 17:45:38 +0200

    refactor: streamline `nuxt.config.ts` by consolidating modules, CSS, and compatibility settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,80 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
 export default defineNuxtConfig({
-  devtools: {
-    enabled: true,
-    timeline: {
-      enabled: true
-    }
-  },
-  routeRules: {
-    '/': { prerender: true },
-  },
+    compatibilityDate: '2025-05-15',
 
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
-    layoutTransition: { name: 'page', mode: 'out-in' },
-    head: {
-      title: 'Dein Büro für Entwicklung und Design – JOTT.MEDIA',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        {
-          name: 'description',
-          content: 'Wir unterstützen dich und dein Unternehmen eigene Prozesse digital zu vereinfachen, um mehr Zeit, mehr Zufriedenheit und mehr Qualität zu gewinnen und erfoglreich zu wachsen.'
-        },
-        { name: 'format-detection', content: 'telephone=no' },
-        { name: 'msapplication-TileColor', content: '#ffffff' },
-        { name: 'theme-color', content: '#ffffff' },
-      ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        {
-          rel: 'apple-touch-icon',
-          sizes: '180x180',
-          href: '/apple-touch-icon.png',
-        },
-        {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '32x32',
-          href: '/favicon-32x32.png',
-        },
-        {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '16x16',
-          href: '/favicon-16x16.png',
-        },
-        { rel: 'manifest', href: '/site.webmanifest' },
-        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
-      ]
+    future: {
+        compatibilityVersion: 4,
     },
-  },
 
-  extends: ['@nuxt/ui-pro'],
-
-  modules: [
-    '@nuxt/content',
-    '@nuxt/ui',
-    '@vueuse/nuxt',
-    '@nuxt/image',
-  ],
-  pages: true,
-  colorMode: {
-    preference: 'light'
-  },
-  uiPro: { routerOptions: false },
-  content: {
-    preview: {
-      api: 'https://api.nuxt.studio',
-      gitInfo: {
-        name: 'jottmedia.github.io',
-        owner: 'JOTTMEDIA',
-        url: 'https://github.com/JOTTMEDIA/jottmedia.github.io'
-      }
-    }
-  },
-
-  compatibilityDate: '2025-01-13'
+    css: ['~/assets/css/main.css'],
+    modules: ['@nuxt/ui-pro', '@nuxt/content', '@pinia/nuxt','@nuxt/image'],
+    devtools: {enabled: true}
 })
