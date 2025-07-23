2be50a9f1519545891f5367be754603d3ca00b71
c1chy-jott <paul@jott.media>
2025-07-23 18:47:58 +0200

    chore: update footer layout with new slot structure and visual enhancements
    Refactored `default.vue` to include additional footer slots, updated styles, and integrated new visual elements like `footer-top.svg` and `footer-box.png` for improved design consistency. Simplified footer slot configuration in `app.config.ts`.

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,12 +3,10 @@ export default defineAppConfig({
         footer: {
             slots: {
                 root: '',
-                top: 'lg:pt-4',
+                top: 'lg:pt-4 contents',
                 bottom: 'py-8 lg:py-0',
-                container: 'py-8 lg:py-0 lg:flex lg:items-center lg:justify-between lg:gap-x-3',
-                left: 'flex items-center justify-center lg:justify-start lg:flex-1 gap-x-1.5 mt-3 lg:mt-0 lg:order-1',
-                center: 'mt-3 lg:mt-0 lg:order-2 flex items-center justify-center',
-                right: 'lg:flex-1 flex items-center justify-center lg:justify-end gap-x-1.5 lg:order-3'
+                left: 'max-w-(--container-5xl)',
+                right: ''
             }
         },
         blogPost: {
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,52 +1,58 @@
 <template>
-  <div class="relative z-10">
-    <slot/>
-    <UFooter>
-      <template #top>
-        <UContainer class="relative z-10 py-0  max-w-(--container-2xl)">
-          <div class="flex justify-between items-end">
-            <div>
-              <address class="not-italic">
-                JOTT.MEDIA GmbH<br>
-                Bahnhofstraße 33<br>
-                31675 Bückeburg<br>
-                <br>
-                <nuxt-link href="tel:+4957229184984">+49 5722 9184984</nuxt-link>
-                <br>
-                <nuxt-link href="mailto:hallo@jott.media">hallo@jott.media</nuxt-link>
-              </address>
-            </div>
-            <div class="text-right">
-              <nuxt-link
-                  class="block font-[800] text-(--color-jm-primary-green) uppercase"
-                  to="/imprint">.Impressum
-              </nuxt-link>
-              <nuxt-link
-                  class="block font-[800] text-(--color-jm-primary-green) uppercase"
-                  to="/privacy">.Datenschutz
-              </nuxt-link>
-            </div>
+  <slot/>
+  <UFooter class="z-10 relative py-0">
+    <template #top>
+      <UContainer class="py-0 max-w-(--container-2xl)">
+        <div class="flex justify-between items-end py-0">
+          <div>
+            <address class="not-italic">
+              JOTT.MEDIA GmbH<br>
+              Bahnhofstraße 33<br>
+              31675 Bückeburg<br>
+              <br>
+              <nuxt-link href="tel:+4957229184984">+49 5722 9184984</nuxt-link>
+              <br>
+              <nuxt-link href="mailto:hallo@jott.media">hallo@jott.media</nuxt-link>
+            </address>
           </div>
-        </UContainer>
-      </template>
-      <!--      <UNavigationMenu
-
-                :arrow="false"
-
-                :items="items" class=""
-                orientation="vertical"
-            />-->
-      <template #bottom>
-        <div class=" bg-(--color-jm-primary-green) text-sm py-5">
-          <UContainer class="max-w-(--container-2xl)">
-            <p>© {{ new Date().getFullYear() }} JOTT.MEDIA – Alle Rechte vorbehalten</p>
-          </UContainer>
+          <div class="text-right">
+            <nuxt-link
+                class="block font-[800] text-(--color-jm-primary-green) uppercase"
+                to="/imprint">.Impressum
+            </nuxt-link>
+            <nuxt-link
+                class="block font-[800] text-(--color-jm-primary-green) uppercase"
+                to="/privacy">.Datenschutz
+            </nuxt-link>
+          </div>
         </div>
-      </template>
+      </UContainer>
+    </template>
+    <template #right>
+      <NuxtImg
+          alt="Footer Bottom"
+          class="absolute pointer-events-none object-contain w-96 bottom-16 right-0 -z-1 "
+          format="webp"
+          src="/images/footer-box.png"
+      />
+    </template>
+    <template #bottom>
+      <div class=" bg-(--color-jm-primary-green) text-xl py-6 flex items-center justify-center">
+        <UContainer class="max-w-(--container-2xl) ">
+          <p>© {{ new Date().getFullYear() }} JOTT.MEDIA – Alle Rechte vorbehalten</p>
+        </UContainer>
+      </div>
+    </template>
+    <template #left>
+      <div class="absolute left-0 bottom-14 w-full h-full bg-(--color-jm-secondary-grey-lighter)  -z-20"></div>
 
-    </UFooter>
-  </div>
-
+      <NuxtImg
+          alt="Footer Top"
+          class="absolute left-1/2 -top-24 bg-(--color-j m-secondary-white) -translate-x-1/2 -z-10 h-[58px] w-full"
+          src="/images/footer-top.svg"
+      />
+    </template>
+  </UFooter>
 </template>
 <script lang="ts" setup>
 import type {NavigationMenuItem} from '@nuxt/ui'
