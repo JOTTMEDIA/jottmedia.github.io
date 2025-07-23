73615fc2283d2b25580d9584812866c186c0dce2
c1chy-jott <paul@jott.media>
2025-07-23 16:03:18 +0200

    chore: clean up nuxt.config.ts by removing redundant future section and fixing module spacing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-05-15',
-
-    future: {
-        compatibilityVersion: 4,
-    },
-
     css: ['~/assets/css/main.css'],
-    modules: ['@nuxt/ui-pro', '@nuxt/content', '@pinia/nuxt','@nuxt/image'],
+    modules: ['@nuxt/ui-pro', '@nuxt/content', '@pinia/nuxt', '@nuxt/image'],
     devtools: {enabled: true}
 })
